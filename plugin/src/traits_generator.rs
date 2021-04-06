dc284cb5b1eb288aa20ac55c18d22fcf7e182b8f
Shohei Wada <pc@wada314.jp>
2021-04-07 00:27:40 +0900

    making...

--- plugin/src/traits_generator.rs
+++ plugin/src/traits_generator.rs
@@ -1,6 +1,17 @@
-use crate::plugin;
+use crate::plugin::*;
 use crate::Result;
+use ::itertools::Itertools;
 
-pub(crate) fn generate_traits(cgreq: &plugin::CodeGeneratorRequest) -> Result<String> {
-    Ok("Hogehoge".to_string())
+fn generate_traits_for_descriptor(desc: &DescriptorProto) -> Result<String> {
+    Ok(desc.name.clone())
+}
+
+pub(crate) fn generate_traits(cgreq: &CodeGeneratorRequest) -> Result<String> {
+    cgreq
+        .proto_file
+        .iter()
+        .map(|fdp| &fdp.message_type)
+        .flatten()
+        .map(|desc| generate_traits_for_descriptor(desc))
+        .fold_ok(String::new(), |l, r| l + &r)
 }
