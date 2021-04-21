3b1b471082b80ca4b2bb0295e4873851e3232368
Shohei Wada <pc@wada314.jp>
2021-04-21 19:34:53 +0900

    eeyan?

--- puroro/src/deser.rs
+++ puroro/src/deser.rs
@@ -1,3 +1,4 @@
 mod bytes;
 mod iters;
+mod slice;
 use iters::{BytesIterator, CharsIterator, VariantsIterator};
---
+++ puroro/src/deser/slice.rs
@@ -0,0 +1,90 @@
+use std::convert::TryInto;
+use std::io::Read;
+
+use super::iters::{CharsIterator, VariantsIterator};
+use super::BytesIterator;
+use crate::types::{FieldData, WireType};
+use crate::variant::Variant;
+use crate::PuroroError;
+use crate::Result;
+use ::num_traits::FromPrimitive;
+
+pub trait BytesSlice: Sized + std::io::Read {
+    fn deser_message<H: DeserializeMessageFromSliceEventHandler>(
+        &mut self,
+        handler: &mut H,
+    ) -> Result<()>;
+}
+impl<'a> BytesSlice for &'a [u8] {
+    fn deser_message<H: DeserializeMessageFromSliceEventHandler>(
+        &mut self,
+        handler: &mut H,
+    ) -> Result<()> {
+        while let Some((new_slice, wire_type, field_number)) =
+            try_get_wire_type_and_field_number(self)?
+        {
+            *self = new_slice;
+            let field_data = match wire_type {
+                WireType::Variant => {
+                    let variant = Variant::decode_bytes(&mut self.bytes())?;
+                    FieldData::Variant(variant)
+                }
+                WireType::LengthDelimited => {
+                    let field_length = Variant::decode_bytes(&mut self.bytes())?.to_usize()?;
+                    let (inner_slice, rest) = self.split_at(field_length);
+                    *self = rest;
+                    FieldData::LengthDelimited(inner_slice)
+                }
+                WireType::Bits32 => {
+                    if self.len() < 4 {
+                        Err(PuroroError::UnexpectedInputTermination)?;
+                    }
+                    let (bytes, rest) = self.split_at(4);
+                    *self = rest;
+                    FieldData::Bits32(
+                        bytes
+                            .try_into()
+                            .map_err(|_| PuroroError::UnexpectedInputTermination)?,
+                    )
+                }
+                WireType::Bits64 => {
+                    if self.len() < 8 {
+                        Err(PuroroError::UnexpectedInputTermination)?;
+                    }
+                    let (bytes, rest) = self.split_at(8);
+                    *self = rest;
+                    FieldData::Bits64(
+                        bytes
+                            .try_into()
+                            .map_err(|_| PuroroError::UnexpectedInputTermination)?,
+                    )
+                }
+                WireType::StartGroup | WireType::EndGroup => Err(PuroroError::GroupNotSupported)?,
+            };
+            handler.met_field(field_data, field_number)?;
+        }
+        Ok(())
+    }
+}
+
+pub trait DeserializableFromBytes {
+    fn deserialize(slice: &[u8]) -> Self;
+}
+
+pub trait DeserializeMessageFromSliceEventHandler {
+    fn met_field(&mut self, field: FieldData<&[u8]>, field_number: usize) -> Result<()>;
+}
+
+fn try_get_wire_type_and_field_number(
+    mut slice: &[u8],
+) -> Result<Option<(&[u8], WireType, usize)>> {
+    if slice.len() == 0 {
+        return Ok(None);
+    }
+    let key = { Variant::decode_bytes(&mut slice.bytes())?.to_usize()? };
+    Ok(Some((
+        slice,
+        WireType::from_usize(key & 0x07).ok_or(PuroroError::InvalidWireType)?,
+        (key >> 3),
+    )))
+}
