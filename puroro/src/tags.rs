006b00e612ca068065da49f2a4f35533b9bb63ac
Shohei Wada <pc@wada314.jp>
2021-04-25 18:46:33 +0900

    new names for proto3 optionals

--- puroro/src/tags.rs
+++ puroro/src/tags.rs
@@ -42,10 +42,14 @@ impl FieldTypeTag for SFixed64 {}
 
 pub trait FieldLabelTag {}
 pub struct Repeated;
-pub struct Optional;
+pub struct Optional2;
+pub struct Optional3NoPresence; // Proto3 unlabeled field.
+pub struct Optional3ExplicitPresence; // Proto3 explicitly optional marked field.
 pub struct Required;
 impl FieldLabelTag for Repeated {}
-impl FieldLabelTag for Optional {}
+impl FieldLabelTag for Optional2 {}
+impl FieldLabelTag for Optional3NoPresence {}
+impl FieldLabelTag for Optional3ExplicitPresence {}
 impl FieldLabelTag for Required {}
 
 impl VariantTypeTag for Int32 {}
