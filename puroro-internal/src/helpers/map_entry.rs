29e3fd73087c9ddb91a1f82b1e43def92d4fc7ac
Shohei Wada <pc@wada314.jp>
2021-04-28 17:45:57 +0900

    damesou

--- puroro-internal/src/helpers/map_entry.rs
+++ puroro-internal/src/helpers/map_entry.rs
@@ -34,35 +34,3 @@ where
         }
     }
 }
-
-impl<KT, VT, KR, VR> DeserializableMessageFromIter for MapEntry<KT, VT, KR, VR>
-where
-    KT: FieldTypeTag,
-    VT: FieldTypeTag,
-    KR: DeserializableFromIterField<(KT, tags::Required)>,
-    VR: DeserializableFromIterField<(VT, tags::Required)>,
-{
-    fn met_field<'a, 'b, I>(
-        &mut self,
-        field: FieldData<&'a mut BytesIter<'b, I>>,
-        field_number: usize,
-    ) -> Result<()>
-    where
-        I: Iterator<Item = std::io::Result<u8>>,
-    {
-        match field_number {
-            0 => <KR as DeserializableFromIterField<(KT, tags::Required)>>::deser(
-                &mut self.key,
-                field,
-                f,
-            )?,
-            1 => <VR as DeserializableFromIterField<(VT, tags::Required)>>::deser(
-                &mut self.value,
-                field,
-                f,
-            )?,
-            _ => Err(ErrorKind::UnexpectedFieldId)?,
-        }
-        todo!()
-    }
-}
