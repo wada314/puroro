d4608f1b6da899cfe83d150495f76ca1c16c01f6
Shohei Wada <pc@wada314.jp>
2021-05-04 16:33:17 +0900

    cloning

--- puroro-internal/src/helpers/field_clone.rs
+++ puroro-internal/src/helpers/field_clone.rs
@@ -7,3 +7,46 @@ pub trait FieldClone<'bump>: Sized {
         <Self as FieldClone>::clone(self)
     }
 }
+
+#[cfg(feature = "puroro-bumpalo")]
+impl<'bump, T: Clone> FieldClone<'bump> for ::bumpalo::boxed::Box<'bump, T> {
+    fn clone(&self) -> Self {
+        panic!("bumpalo box needs a bumpalo instance to clone!");
+    }
+    #[cfg(feature = "puroro-bumpalo")]
+    fn clone_in_bumpalo(&self, bump: &'bump ::bumpalo::Bump) -> Self {
+        ::bumpalo::boxed::Box::new_in(self.as_ref().clone(), bump)
+    }
+}
+
+macro_rules! define_field_clone {
+    ($ty:ty) => {
+        impl<'bump> FieldClone<'bump> for $ty {
+            fn clone(&self) -> Self {
+                <Self as Clone>::clone(self)
+            }
+        }
+    };
+    ($ty:ty, < $($gp:ident $( : $bound:ident)? ),* >) => {
+        impl<'bump $(, $gp $( : $bound)?)+ > FieldClone<'bump> for $ty {
+            fn clone(&self) -> Self {
+                <Self as Clone>::clone(self)
+            }
+        }
+    };
+}
+define_field_clone!(i32);
+define_field_clone!(i64);
+define_field_clone!(u32);
+define_field_clone!(u64);
+define_field_clone!(f32);
+define_field_clone!(f64);
+define_field_clone!(bool);
+define_field_clone!(String);
+define_field_clone!(std::result::Result<T, i32>, <T: Clone>);
+define_field_clone!(Option<T>, <T: Clone>);
+define_field_clone!(Vec<T>, <T: Clone>);
+#[cfg(feature = "puroro-bumpalo")]
+define_field_clone!(::bumpalo::collections::Vec<'bump, T>, <T: Clone>);
+#[cfg(feature = "puroro-bumpalo")]
+define_field_clone!(::bumpalo::collections::String<'bump>);
