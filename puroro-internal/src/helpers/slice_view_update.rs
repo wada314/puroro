8c29379db1514188e638d34dd32db0e77be9a551
Shohei Wada <pc@wada314.jp>
2021-05-08 23:08:09 +0900

    update...

--- puroro-internal/src/helpers/slice_view_update.rs
+++ puroro-internal/src/helpers/slice_view_update.rs
@@ -1,8 +1,24 @@
+use crate::deser::BytesSlice;
+use crate::tags;
+use crate::tags::FieldTypeTag;
 use crate::types::{FieldData, SliceViewRepeatedField, SliceViewScalarField};
+use crate::variant::Variant;
 use crate::{ErrorKind, Result};
 
-pub trait SliceViewUpdate {
+pub trait SliceViewUpdate<TypeTag: FieldTypeTag> {
     fn update(&mut self, field: FieldData<&[u8]>) -> Result<()>;
 }
 
-
+impl SliceViewUpdate<tags::Int32> for SliceViewScalarField<i32> {
+    fn update(&mut self, field: FieldData<&[u8]>) -> Result<()> {
+        let new_val = match field {
+            FieldData::Variant(variant) => variant.to_native::<tags::Int32>()?,
+            FieldData::LengthDelimited(slice) => {
+                let wrapped_slice = BytesSlice::new(slice);
+                todo!()
+            }
+            _ => Err(ErrorKind::InvalidWireType)?,
+        };
+        Ok(())
+    }
+}
