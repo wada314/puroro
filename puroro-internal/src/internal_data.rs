31d8f17ea782e8f114597b7aa87404d30639336e
Shohei Wada <pc@wada314.jp>
2021-05-17 11:00:43 +0900

    iikanji?

--- puroro-internal/src/internal_data.rs
+++ puroro-internal/src/internal_data.rs
@@ -2,6 +2,7 @@ use crate::deser::LdSlice;
 use crate::types::{FieldData, SliceViewField};
 use crate::{ErrorKind, Result};
 use ::either_n::Either4;
+use itertools::{Either, Itertools};
 use puroro::InternalData;
 use std::collections::HashMap;
 
@@ -90,93 +91,74 @@ impl<'slice, 'p> InternalDataForSliceViewStruct<'slice, 'p> {
         }
     }
 
-    pub fn ld_slices(&self) -> impl Iterator<Item = LdSlice<'slice>> {
-        self.source_ld_slices.iter()
-    }
-}
-impl<'slice, 'p> SourceLdSlices<'slice, 'p> {
-    pub fn iter(&self) -> impl Iterator<Item = LdSlice<'slice>> {
-        match self.clone() {
-            SourceLdSlices::SingleLdSlice(ld_slice) => {
-                Either4::One(std::iter::once(ld_slice.clone()))
-            }
-            SourceLdSlices::MaybeMultipleLdSlices {
-                field_in_parent,
-                field_number_in_parent,
-                parent_internal_data,
-            } => match field_in_parent.cloned() {
-                None => {
-                    // This can happen if a scalar message field exists in .proto definition
-                    // but it does not appear in the serialized input stream.
-                    Either4::Two(std::iter::empty())
-                }
-                Some(SliceViewField::FieldInSingleSlice {
-                    ld_slice, count, ..
-                }) => {
-                    // iterate over a single slice given from a parent message,
-                    // and choose out the fields that has proper field number.
-                    let iter = ld_slice
-                        .fields()
-                        .filter_map(move |rfield| {
-                            let field = rfield.expect(
-                                "An error occured while deserializing the input. \
-                                Consider checking the data in earlier stage to catch this error.",
-                            );
-                            if field.number == field_number_in_parent {
-                                if let FieldData::LengthDelimited(inner_ld_slice) = field.data {
-                                    Some(inner_ld_slice)
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
-                        })
-                        .take(count);
-                    Either4::Three(iter)
-                }
-                Some(SliceViewField::FieldInMultipleSlices {
-                    count,
-                    first_enclosing_ld_slice: first_enclosing_slice,
-                    ..
-                }) => {
-                    // The parent message instance is scattering around multiple slices.
-                    // In this case we need to iterate in the parent message recursively,
-                    // that means we cannot use a static dispatching iterator because the type
-                    // of the iterator recursives infinitely.
-                    let iter = parent_internal_data
-                        .ld_slices()
-                        .skip_while(move |ld_slice| *ld_slice != first_enclosing_slice)
-                        .flat_map(move |ld_slice| {
-                            ld_slice.fields().filter_map(move |rfield| {
-                                let field = rfield.expect(
-                                    "An error occured while deserializing the input. \
-                                Consider checking the data in earlier stage to catch this error.",
-                                );
-                                if field.number == field_number_in_parent {
-                                    if let FieldData::LengthDelimited(inner_ld_slice) = field.data {
-                                        Some(inner_ld_slice)
-                                    } else {
-                                        None
-                                    }
-                                } else {
-                                    None
-                                }
-                            })
-                        })
-                        .take(count);
-                    // Dynamic dispatching iterator to avoid an infinite recursive type...
-                    Either4::Four(Box::new(iter) as Box<dyn Iterator<Item = LdSlice<'slice>>>)
-                }
-            },
-        }
-        .into_iter()
+    pub fn ld_slices(&'p self) -> impl 'p + Iterator<Item = Result<LdSlice<'slice>>> {
+        SourceLdSlicesIter::<'slice, 'p>::new(&self.source_ld_slices, None)
     }
 }
 
 struct SourceLdSlicesIter<'slice, 'p> {
     container: &'p SourceLdSlices<'slice, 'p>,
     prev_ld_slice: Option<LdSlice<'slice>>,
+}
+
+impl<'slice, 'p> Iterator for SourceLdSlicesIter<'slice, 'p> {
+    type Item = Result<LdSlice<'slice>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.try_next().transpose()
+    }
+}
+
+impl<'slice, 'p> SourceLdSlicesIter<'slice, 'p> {
+    fn new(
+        container: &'p SourceLdSlices<'slice, 'p>,
+        prev_ld_slice: Option<LdSlice<'slice>>,
+    ) -> Self {
+        Self {
+            container,
+            prev_ld_slice,
+        }
+    }
+
+    fn try_next(&mut self) -> Result<Option<LdSlice<'slice>>> {
+        let result = match (self.container.clone(), self.prev_ld_slice.clone()) {
+            (SourceLdSlices::SingleLdSlice(slice), None) => Some(slice),
+            (SourceLdSlices::SingleLdSlice(_), Some(_)) => None,
+            (
+                SourceLdSlices::MaybeMultipleLdSlices {
+                    field_in_parent,
+                    field_number_in_parent,
+                    parent_internal_data,
+                },
+                None,
+            ) => {
+                parent_internal_data
+                    .ld_slices()
+                    .map_ok(|ld_slice| ld_slice.fields())
+                    .flatten_ok()
+                    // ↓ i.e. Result<Result<T, E>, E>::flatten() => Result<T, E>, which is not stable yet.
+                    .map(|rrfield| rrfield.and_then(|x| x))
+                    .filter_map_ok(|field| {
+                        if field.number == field_number_in_parent {
+                            if let FieldData::LengthDelimited(ld_slice) = field.data {
+                                return Some(ld_slice);
+                            }
+                        }
+                        None
+                    });
+                todo!()
+            }
+            (
+                SourceLdSlices::MaybeMultipleLdSlices {
+                    field_in_parent,
+                    field_number_in_parent,
+                    parent_internal_data,
+                },
+                Some(_),
+            ) => {}
+        };
+        self.prev_ld_slice = result.or(self.prev_ld_slice);
+        result
+    }
 }
 
 impl<'bump, 'slice, 'p> InternalData<'bump> for InternalDataForSliceViewStruct<'slice, 'p> {
