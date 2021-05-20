08bf2cfc00263faa14444d3057c588c7802a4509
Shohei Wada <pc@wada314.jp>
2021-05-20 10:17:39 +0900

    repeated...

--- puroro-internal/src/helpers/repeated_slice_view.rs
+++ puroro-internal/src/helpers/repeated_slice_view.rs
@@ -1,11 +1,12 @@
 use std::marker::PhantomData;
 
-use crate::deser::LdSlice;
 use crate::tags;
 use crate::types::{FieldData, SliceViewField};
+use crate::variant::VariantTypeTag;
 use crate::InternalDataForSliceViewStruct;
 use crate::{ErrorKind, Result, ResultHelper};
 use ::itertools::Itertools;
+use ::puroro::RepeatedField;
 use itertools::Either;
 
 #[derive(Debug, Clone)]
@@ -20,7 +21,9 @@ where
 }
 
 impl<'slice, 'p> RepeatedSliceViewField<'slice, 'p, tags::Int32> {
-    pub fn iter(&self) -> impl '_ + Iterator<Item = i32> {
+    pub fn iter_impl(
+        &self,
+    ) -> impl '_ + Iterator<Item = <tags::Int32 as VariantTypeTag>::NativeType> {
         self.internal_data
             .field_data_iter(self.field_number, self.field)
             .map_ok(|field| -> Result<_ /* impl Iterator<Item=Result<i32>> */> {
@@ -36,11 +39,38 @@ impl<'slice, 'p> RepeatedSliceViewField<'slice, 'p, tags::Int32> {
                     ),
                     _ => Err(ErrorKind::UnexpectedWireType)?,
                 }
-                .into_iter()) // Result<Iterator<Item=Result<i32>>>
+                .into_iter()) // Result<Iterator<Item=Result<<tags::Int32 as VariantTypeTag>::NativeType>>>
             })
             .map(|rrval| rrval.flatten())
             .flatten_ok()
             .map(|rrval| rrval.flatten())
             .map(|result| result.unwrap())
+    }
+}
+
+impl<'slice, 'p> RepeatedField<i32> for RepeatedSliceViewField<'slice, 'p, tags::Int32> {
+    fn for_each<F>(&self, f: F)
+    where
+        F: FnMut(&i32),
+    {
+        for v in self.iter_impl() {
+            f(&v);
+        }
+    }
+
+    fn boxed_iter(&self) -> Box<dyn '_ + Iterator<Item = std::borrow::Cow<'_, i32>>> {
+        todo!()
+    }
+
+    type Iter<'a>
+    where
+        Self: 'a,
+        i32: 'a + ToOwned;
+
+    fn iter<'a>(&'a self) -> Self::Iter<'a>
+    where
+        i32: 'a + ToOwned,
+    {
+        todo!()
     }
 }
