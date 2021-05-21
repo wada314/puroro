0391169ece9786a8dcb2fb337f403cd6abdb9e02
Shohei Wada <pc@wada314.jp>
2021-05-21 19:00:12 +0900

    remove stage*

--- puroro-plugin/src/main.rs
+++ puroro-plugin/src/main.rs
@@ -5,12 +5,7 @@
 mod context;
 mod error;
 mod generators;
-#[cfg(feature = "stage1")]
-mod stage1;
-#[cfg(feature = "stage2")]
-mod stage2;
-#[cfg(feature = "stage3")]
-mod stage3;
+mod protos;
 mod utils;
 mod wrappers;
 
@@ -24,18 +19,10 @@ type Result<T> = std::result::Result<T, GeneratorError>;
 use std::io::Read;
 use std::io::{stdin, stdout};
 
-mod protos {
-    #[cfg(feature = "stage1")]
-    pub use crate::stage1::*;
-    #[cfg(feature = "stage2")]
-    pub use crate::stage2::google;
-    #[cfg(feature = "stage3")]
-    pub use crate::stage3::google;
-}
-use google::protobuf::compiler::{
+pub use protos::google;
+use protos::google::protobuf::compiler::{
     code_generator_response, CodeGeneratorRequest, CodeGeneratorResponse,
 };
-pub use protos::google;
 
 fn main() -> Result<()> {
     let mut cgreq = CodeGeneratorRequest::default();
