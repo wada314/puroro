b5d59d47446d179dc2c411b56b1ab80d928aaa2a
Shohei Wada <pc@wada314.jp>
2021-05-21 22:57:58 +0900

    update tests crate

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,12 +1,3 @@
-#![feature(allocator_api)]
-#![feature(generic_associated_types)]
-#![feature(min_type_alias_impl_trait)]
-
-//#[path = r"D:\Documents\projects\proto-rust\target\debug\build\tests-71fe3c39c5dbebf6\out\mod.rs"]
-//mod protos;
-
-include!(r"D:\Documents\projects\proto-rust\target\debug\build\tests-71fe3c39c5dbebf6\out\mod.rs");
-
 #[cfg(test)]
 mod tests {
     use super::*;
