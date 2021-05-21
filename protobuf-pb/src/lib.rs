23afb8a7557518e562b33389d05f0a5efd482292
Shohei Wada <pc@wada314.jp>
2021-05-21 22:42:48 +0900

    new standalone proto package

---
+++ protobuf-pb/src/lib.rs
@@ -0,0 +1,4 @@
+#![cfg_attr(feature = "puroro-nightly", feature(generic_associated_types))]
+#![cfg_attr(feature = "puroro-nightly", feature(min_type_alias_impl_trait))]
+
+include!(concat!(env!("OUT_DIR"), "/mod.rs"));
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -2,10 +2,17 @@
 #![feature(generic_associated_types)]
 #![feature(min_type_alias_impl_trait)]
 
+//#[path = r"D:\Documents\projects\proto-rust\target\debug\build\tests-71fe3c39c5dbebf6\out\mod.rs"]
+//mod protos;
+
+include!(r"D:\Documents\projects\proto-rust\target\debug\build\tests-71fe3c39c5dbebf6\out\mod.rs");
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
+        use google::protobuf::DescriptorProto;
         assert_eq!(2 + 2, 4);
     }
 }
