17c5cd87c466ce3da4d19e0544a7e1ed90cdf82c
Shohei Wada <pc@wada314.jp>
2021-05-23 12:45:51 +0900

    Revert "clean lifetime"
    This reverts commit ca47e98286cf048770b098bc5ae8df76e762e1d6.

--- puroro-plugin/src/syn.rs
+++ puroro-plugin/src/syn.rs
@@ -4,34 +4,26 @@ use std::fmt::Display;
 use std::iter::FromIterator;
 
 #[derive(Debug, Clone)]
-pub struct Ident<'a>(Cow<'a, str>);
-impl<'a> Ident<'a> {
-    pub fn new(s: Cow<'a, str>) -> Self {
+pub struct Ident(Cow<'static, str>);
+impl Ident {
+    pub fn new(s: Cow<'static, str>) -> Self {
         Self(s)
     }
 }
-impl Display for Ident<'_> {
+impl Display for Ident {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(self.0.as_ref())
     }
 }
-impl<'a, T> From<T> for Ident<'a>
-where
-    Cow<'a, str>: From<T>,
-{
-    fn from(s: T) -> Self {
-        Self(s.into())
-    }
-}
 
 #[derive(Debug, Clone)]
-pub struct GenericParams<'a>(Vec<Cow<'a, str>>);
-impl<'a> GenericParams<'a> {
+pub struct GenericParams(Vec<Cow<'static, str>>);
+impl GenericParams {
     pub fn new_empty() -> Self {
         Self(vec![])
     }
 
-    pub fn bind_checked(&self, from: &str, to: Cow<'a, str>) -> Result<Self> {
+    pub fn bind_checked(&self, from: &str, to: Cow<'static, str>) -> Result<GenericParams> {
         let mut newvec = Cow::Borrowed(&self.0);
         for i in 0..newvec.len() {
             if newvec[i] == from {
@@ -47,14 +39,14 @@ impl<'a> GenericParams<'a> {
         }
     }
 
-    pub fn bind_unchecked(&self, from: &str, to: Cow<'a, str>) -> Cow<'_, Self> {
+    pub fn bind_unchecked(&self, from: &str, to: Cow<'static, str>) -> Cow<'_, Self> {
         match self.bind_checked(from, to) {
             Ok(gp) => Cow::Owned(gp),
             Err(_) => Cow::Borrowed(self),
         }
     }
 }
-impl Display for GenericParams<'_> {
+impl Display for GenericParams {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> std::fmt::Result {
         use ::std::fmt::Write;
         let mut iter = self.0.iter();
@@ -70,9 +62,9 @@ impl Display for GenericParams<'_> {
         Ok(())
     }
 }
-impl<'a, T> FromIterator<T> for GenericParams<'a>
+impl<T> FromIterator<T> for GenericParams
 where
-    Cow<'a, str>: From<T>,
+    Cow<'static, str>: From<T>,
 {
     fn from_iter<U: IntoIterator<Item = T>>(iter: U) -> Self {
         Self(iter.into_iter().map(|x| x.into()).collect::<Vec<_>>())
@@ -80,36 +72,36 @@ where
 }
 
 #[derive(Debug, Clone)]
-struct PathItem<'a> {
-    ident: Ident<'a>,
-    gp: GenericParams<'a>,
+struct PathItem {
+    ident: Ident,
+    gp: GenericParams,
 }
-impl<'a> PathItem<'a> {
-    pub fn new(ident: Ident<'a>, gp: GenericParams<'a>) -> Self {
+impl PathItem {
+    pub fn new(ident: Ident, gp: GenericParams) -> Self {
         Self { ident, gp }
     }
-    pub fn bind_checked(&self, from: &str, to: Cow<'a, str>) -> Result<Self> {
+    pub fn bind_checked(&self, from: &str, to: Cow<'static, str>) -> Result<Self> {
         Ok(Self {
             ident: self.ident.clone(),
             gp: self.gp.bind_checked(from, to)?,
         })
     }
-    pub fn bind_unchecked(&self, from: &str, to: Cow<'a, str>) -> Cow<'_, Self> {
+    pub fn bind_unchecked(&self, from: &str, to: Cow<'static, str>) -> Cow<'_, Self> {
         match self.bind_checked(from, to) {
             Ok(item) => Cow::Owned(item),
             Err(_) => Cow::Borrowed(self),
         }
     }
 }
-impl<'a> From<Ident<'a>> for PathItem<'a> {
-    fn from(ident: Ident<'a>) -> Self {
+impl From<Ident> for PathItem {
+    fn from(ident: Ident) -> Self {
         Self {
             ident,
             gp: GenericParams::new_empty(),
         }
     }
 }
-impl Display for PathItem<'_> {
+impl Display for PathItem {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{}", self.ident))?;
         if !self.gp.0.is_empty() {
@@ -120,9 +112,9 @@ impl Display for PathItem<'_> {
 }
 
 #[derive(Debug, Clone)]
-struct PathExpr<'a>(Vec<PathItem<'a>>);
-impl<'a> PathExpr<'a> {
-    pub fn bind_checked(&self, from: &str, to: Cow<'a, str>) -> Result<Self> {
+struct PathExpr(Vec<PathItem>);
+impl PathExpr {
+    pub fn bind_checked(&self, from: &str, to: Cow<'static, str>) -> Result<Self> {
         let mut newvec = Cow::Borrowed(&self.0);
         for i in 0..self.0.len() {
             if let Ok(new_item) = self.0[i].bind_checked(from, to.clone()) {
@@ -136,14 +128,14 @@ impl<'a> PathExpr<'a> {
             Cow::Owned(vec) => Ok(Self(vec)),
         }
     }
-    pub fn bind_unchecked(&self, from: &str, to: Cow<'a, str>) -> Cow<'_, Self> {
+    pub fn bind_unchecked(&self, from: &str, to: Cow<'static, str>) -> Cow<'_, Self> {
         match self.bind_checked(from, to) {
             Ok(path) => Cow::Owned(path),
             Err(_) => Cow::Borrowed(self),
         }
     }
 }
-impl Display for PathExpr<'_> {
+impl Display for PathExpr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut iter = self.0.iter();
         if let Some(first) = iter.next() {
@@ -155,8 +147,8 @@ impl Display for PathExpr<'_> {
         Ok(())
     }
 }
-impl<'a> FromIterator<PathItem<'a>> for PathExpr<'a> {
-    fn from_iter<T: IntoIterator<Item = PathItem<'a>>>(iter: T) -> Self {
+impl FromIterator<PathItem> for PathExpr {
+    fn from_iter<T: IntoIterator<Item = PathItem>>(iter: T) -> Self {
         Self(Vec::from_iter(iter))
     }
 }
