779de0df4e2942178ccb97b3705126d924e3de55
Shohei Wada <pc@wada314.jp>
2021-05-23 15:09:35 +0900

    redirect

--- puroro-plugin/src/main.rs
+++ puroro-plugin/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(feature = "puroro-nightly", feature(backtrace))]
 #![cfg_attr(feature = "puroro-nightly", feature(generic_associated_types))]
 #![cfg_attr(feature = "puroro-nightly", feature(min_type_alias_impl_trait))]
+#![allow(incomplete_features)]
 
 mod context;
 mod error;
