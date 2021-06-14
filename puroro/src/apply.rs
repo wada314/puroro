9ca1c0ef753b1cee18085e168a0cea8e864df340
Shohei Wada <pc@wada314.jp>
2021-06-14 16:20:38 +0900

    maybe...move <T> of tags::Enum and Message into associated types?

--- puroro/src/apply.rs
+++ puroro/src/apply.rs
@@ -8,3 +8,12 @@ pub trait ApplyToFieldMutForSimpleMessage<ReturnType, ErrorType>:
     ApplyToFieldMut<tags::Required, tags::Int32, i32, ReturnType, ErrorType>
 {
 }
+
+macro_rules! simple_message_triples {
+    () => {
+        (
+            (tags::Required, tags::Int32, i32),
+            (tags::Required, tags::String, String),
+        )
+    };
+}
