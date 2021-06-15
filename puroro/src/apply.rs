cfd9613a03d82a0b88be92bbeb427e95214b5a06
Shohei Wada <pc@wada314.jp>
2021-06-15 17:16:30 +0900

    aho

--- puroro/src/apply.rs
+++ puroro/src/apply.rs
@@ -1,5 +1,5 @@
 pub trait ApplyToField<LabelTag, WireAndValueTag, FieldType, ReturnType, ErrorType> {
-    fn apply(&self, field: &mut FieldType) -> Result<ReturnType, ErrorType>;
+    fn apply(&mut self, field: &FieldType) -> Result<ReturnType, ErrorType>;
 }
 
 pub trait ApplyToFieldMut<LabelTag, WireAndValueTag, FieldType, ReturnType, ErrorType> {
