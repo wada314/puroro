7c10d9adab5bcb9ebfb41242070338a698b1e846
Shohei Wada <pc@wada314.jp>
2021-06-15 16:12:37 +0900

    damekamo how should I handle enum and msg tags??

--- puroro/src/apply.rs
+++ puroro/src/apply.rs
@@ -4,16 +4,22 @@ pub trait ApplyToFieldMut<LabelTag, WireAndValueTag, FieldType, ReturnType, Erro
     fn apply(&mut self, field: &mut FieldType) -> Result<ReturnType, ErrorType>;
 }
 
-pub trait ApplyToFieldMutForSimpleMessage<ReturnType, ErrorType>:
-    ApplyToFieldMut<tags::Required, tags::Int32, i32, ReturnType, ErrorType>
-{
-}
-
-macro_rules! simple_message_triples {
-    () => {
-        (
-            (tags::Required, tags::Int32, i32),
-            (tags::Required, tags::String, String),
-        )
+macro_rules! define_apply_to_field_for_simple_message {
+    ($(($ltag:ty, $vtag:ty, $actual:ty)),* $(,)?) => {
+        pub trait ApplyToFieldMutForSimpleMessage<ReturnType, ErrorType>:
+            $( ApplyToFieldMut <$ltag, $vtag, $actual, ReturnType, ErrorType> +)*
+        {
+        }
     };
 }
+
+define_apply_to_field_for_simple_message!(
+    (tags::Required, tags::Int32, i32),
+    (tags::Required, tags::UInt32, u32),
+    (tags::Required, tags::SInt32, i32),
+    (tags::Required, tags::Int64, i64),
+    (tags::Required, tags::UInt64, u64),
+    (tags::Required, tags::SInt64, i64),
+    (tags::Required, tags::Bool, bool),
+    (tags::Required, tags::String, String),
+);
