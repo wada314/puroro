1a74ab97d018e904206a347442c68229b0a93569
Shohei Wada <pc@wada314.jp>
2021-06-15 17:15:23 +0900

    uum...

--- puroro/src/apply.rs
+++ puroro/src/apply.rs
@@ -1,25 +1,7 @@
-use crate::tags;
+pub trait ApplyToField<LabelTag, WireAndValueTag, FieldType, ReturnType, ErrorType> {
+    fn apply(&self, field: &mut FieldType) -> Result<ReturnType, ErrorType>;
+}
 
 pub trait ApplyToFieldMut<LabelTag, WireAndValueTag, FieldType, ReturnType, ErrorType> {
-    fn apply(&mut self, field: &mut FieldType) -> Result<ReturnType, ErrorType>;
+    fn apply_mut(&mut self, field: &mut FieldType) -> Result<ReturnType, ErrorType>;
 }
-
-macro_rules! define_apply_to_field_for_simple_message {
-    ($(($ltag:ty, $vtag:ty, $actual:ty)),* $(,)?) => {
-        pub trait ApplyToFieldMutForSimpleMessage<ReturnType, ErrorType>:
-            $( ApplyToFieldMut <$ltag, $vtag, $actual, ReturnType, ErrorType> +)*
-        {
-        }
-    };
-}
-
-define_apply_to_field_for_simple_message!(
-    (tags::Required, tags::Int32, i32),
-    (tags::Required, tags::UInt32, u32),
-    (tags::Required, tags::SInt32, i32),
-    (tags::Required, tags::Int64, i64),
-    (tags::Required, tags::UInt64, u64),
-    (tags::Required, tags::SInt64, i64),
-    (tags::Required, tags::Bool, bool),
-    (tags::Required, tags::String, String),
-);
