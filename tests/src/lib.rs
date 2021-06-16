132a8a9f3213ca0d8cbdb6b653da55c817e34de0
Shohei Wada <pc@wada314.jp>
2021-06-16 17:55:13 +0900

    testing...

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -8,41 +8,43 @@
 
 use ::puroro::apply::ApplyToField;
 use ::puroro::tags;
-use msg::{SubMsg, SubMsgTag, TheMapEntry, TheMapEntryTag};
 
 pub struct Msg {
-    pub the_map: ::std::vec::Vec<self::msg::TheMapEntry>,
+    pub the_map: ::std::vec::Vec<self::TheMapEntry>,
     puroro_internal: ::puroro_internal::InternalDataForNormalStruct,
 }
 pub struct MsgTag();
 impl ::puroro::MessageTag for MsgTag {}
 impl ::puroro::IsMessageImplOfTag<MsgTag> for Msg {}
 
-pub mod msg {
-    #[derive(Debug)]
-    pub struct TheMapEntry {
-        pub key: ::std::string::String,
-        pub value: ::std::option::Option<::std::boxed::Box<self::SubMsg>>,
-        puroro_internal: ::puroro_internal::InternalDataForNormalStruct,
-    }
-    pub struct TheMapEntryTag();
-    impl ::puroro::MessageTag for TheMapEntryTag {}
-    impl ::puroro::IsMessageImplOfTag<TheMapEntryTag> for TheMapEntry {}
+#[derive(Debug)]
+pub struct TheMapEntry {
+    pub key: ::std::string::String,
+    pub value: ::std::option::Option<::std::boxed::Box<self::SubMsg>>,
+    puroro_internal: ::puroro_internal::InternalDataForNormalStruct,
+}
+pub struct TheMapEntryTag();
+impl ::puroro::MessageTag for TheMapEntryTag {}
+impl ::puroro::IsMessageImplOfTag<TheMapEntryTag> for TheMapEntry {}
 
-    #[derive(Debug)]
-    pub struct SubMsg {
-        pub sub_msg: ::std::option::Option<::std::boxed::Box<self::SubMsg>>,
-        puroro_internal: ::puroro_internal::InternalDataForNormalStruct,
-    }
-    pub struct SubMsgTag();
-    impl ::puroro::MessageTag for SubMsgTag {}
-    impl ::puroro::IsMessageImplOfTag<SubMsgTag> for SubMsgTag {}
-} // mod msg
+#[derive(Debug)]
+pub struct SubMsg {
+    pub sub_msg: ::std::option::Option<::std::boxed::Box<self::SubMsg>>,
+    puroro_internal: ::puroro_internal::InternalDataForNormalStruct,
+}
+pub struct SubMsgTag();
+impl ::puroro::MessageTag for SubMsgTag {}
+impl ::puroro::IsMessageImplOfTag<SubMsgTag> for SubMsgTag {}
 
 struct Visitor();
 
-impl ApplyToField<tags::Repeated, tags::Message<MsgTag>, Vec<Msg>, (), ()> for Visitor {
-    fn apply(&mut self, _: &Vec<Msg>, _: usize) -> Result<(), ()> {
+impl<MTag, MType> ApplyToField<tags::Repeated, tags::Message<MTag>, Vec<MType>, (), ()> for Visitor
+where
+    MTag: ::puroro::MessageTag,
+    MType: ::puroro::IsMessageImplOfTag<MTag>,
+{
+    fn apply(&mut self, field: &Vec<MType>, _: usize) -> Result<(), ()> {
+        for item in field {}
         Ok(())
     }
 }
@@ -50,9 +52,20 @@ impl ApplyToField<tags::Repeated, tags::Message<MsgTag>, Vec<Msg>, (), ()> for V
 impl Msg {
     fn apply_to_fields<F, E>(&self, f: &mut F) -> Result<(), E>
     where
-        F: ApplyToField<tags::Repeated, tags::Message<TheMapEntryTag>, Vec<TheMapEntry>, (), E>,
+        F: AppliableToMsgFields<E>,
     {
         f.apply(&self.the_map, 1)?;
+        Ok(())
+    }
+}
+
+impl TheMapEntry {
+    fn apply_to_fields<F, E>(&self, f: &mut F) -> Result<(), E>
+    where
+        F: AppliableToTheMapEntryFields<E>,
+    {
+        f.apply(&self.key, 1)?;
+        f.apply(&self.value, 2)?;
         Ok(())
     }
 }
@@ -63,7 +76,7 @@ trait AppliableToMsgFields<E>:
 }
 
 trait AppliableToTheMapEntryFields<E>:
-    ApplyToField<tags::Optional3, tags::Message<SubMsgTag>, Option<SubMsg>, (), E>
+    ApplyToField<tags::Optional3, tags::Message<SubMsgTag>, Option<Box<SubMsg>>, (), E>
     + ApplyToField<tags::Optional3, tags::String, String, (), E>
 {
 }
@@ -72,8 +85,6 @@ trait AppliableToSubMsgFields<E>:
     ApplyToField<tags::Optional3, tags::Message<SubMsgTag>, Option<SubMsg>, (), E>
 {
 }
-
-trait RecursivelyAppliableToSubMsgFields<E>: RecursivelyAppliableToSubMsgFields<E> {}
 
 #[cfg(test)]
 mod tests {
