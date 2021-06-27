6f7ba511458a2e08ba4afeca189664bd8eec341b
Shohei Wada <pc@wada314.jp>
2021-06-27 17:11:56 +0900

    clear puroro-plugin

--- puroro-plugin/src/main.rs
+++ puroro-plugin/src/main.rs
@@ -3,53 +3,27 @@
 #![cfg_attr(feature = "puroro-nightly", feature(min_type_alias_impl_trait))]
 #![allow(incomplete_features)]
 
-mod context;
 mod error;
-mod generators;
 mod protos;
 mod utils;
-mod wrappers;
 
 use ::puroro::Serializable;
 use ::puroro_internal::deser::MergeableMessageFromIter;
-use context::Context;
 
-use error::{ErrorKind, GeneratorError};
+use error::GeneratorError;
 type Result<T> = std::result::Result<T, GeneratorError>;
 
 use std::io::Read;
 use std::io::{stdin, stdout};
 
 pub use protos::simple::google;
-use protos::simple::google::protobuf::compiler::{
-    code_generator_response, CodeGeneratorRequest, CodeGeneratorResponse,
-};
+use protos::simple::google::protobuf::compiler::{CodeGeneratorRequest, CodeGeneratorResponse};
 
 fn main() -> Result<()> {
     let mut cgreq = CodeGeneratorRequest::default();
     cgreq.merge_from_iter(&mut stdin().bytes()).unwrap();
-    let context = Context::new(
-        &cgreq,
-        context::ImplType::Default,
-        context::AllocatorType::Default,
-    );
-    let filename_and_content = generators::do_generate(&context)?;
+
     let mut cgres = CodeGeneratorResponse::default();
-    cgres.file = filename_and_content
-        .into_iter()
-        .map(|(filename, content)| {
-            let mut file = code_generator_response::File::default();
-            file.name = Some(filename);
-            file.content = Some(content);
-            file
-        })
-        .collect();
     cgres.serialize(&mut stdout())?;
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test() {}
 }
