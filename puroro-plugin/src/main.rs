8ac0a16c7b337474dae79d5a293d6fdad2d62a68
Shohei Wada <pc@wada314.jp>
2021-06-27 23:04:05 +0900

    basic file structure

--- puroro-plugin/src/main.rs
+++ puroro-plugin/src/main.rs
@@ -10,18 +10,67 @@ mod wrappers;
 
 use ::puroro::Serializable;
 use ::puroro_internal::deser::MergeableMessageFromIter;
+use itertools::Itertools;
 
 use error::GeneratorError;
 type Result<T> = std::result::Result<T, GeneratorError>;
 
+use std::collections::{HashMap, HashSet};
 use std::io::Read;
 use std::io::{stdin, stdout};
 
+use google::protobuf::compiler::code_generator_response::File;
+use google::protobuf::compiler::{CodeGeneratorRequest, CodeGeneratorResponse};
+use google::protobuf::FileDescriptorProto;
 pub use protos::simple::google;
-use protos::simple::google::protobuf::compiler::code_generator_response::File;
-use protos::simple::google::protobuf::compiler::{CodeGeneratorRequest, CodeGeneratorResponse};
 
 use askama::Template;
+
+fn make_package_to_subpackages_map(
+    files: &Vec<google::protobuf::FileDescriptorProto>,
+) -> HashMap<String, HashSet<String>> {
+    let mut map = HashMap::new();
+    for file in files {
+        let package_string = file.package.clone().unwrap_or_default();
+        let package_vec = package_string
+            .split('.')
+            .filter_map(|p| {
+                if p.is_empty() {
+                    None
+                } else {
+                    Some(p.to_string())
+                }
+            })
+            .collect::<Vec<_>>();
+        for i in 0..(package_vec.len()) {
+            let cur_package = package_vec.iter().take(i).join(".");
+            let subpackage = package_vec[i].clone();
+            map.entry(cur_package)
+                .or_insert(HashSet::new())
+                .insert(subpackage);
+        }
+    }
+    map
+}
+
+fn package_to_filename(package: &str) -> String {
+    if package.is_empty() {
+        "mod.rs".to_string()
+    } else {
+        package
+            .split('.')
+            .map(|f| utils::get_keyword_safe_ident(f))
+            .join("/")
+            + ".rs"
+    }
+}
+
+#[derive(Template, Default)]
+#[template(path = "mod.rs.txt")]
+pub struct Variables {
+    subpackages: HashSet<String>,
+    file: Option<FileDescriptorProto>,
+}
 
 fn main() -> Result<()> {
     let mut cgreq = CodeGeneratorRequest::default();
@@ -29,11 +78,37 @@ fn main() -> Result<()> {
     let mut cgres = CodeGeneratorResponse::default();
     let mut mod_rs = File::default();
     mod_rs.name = Some("mod.rs".to_string());
+    let package_to_subpackage_map = make_package_to_subpackages_map(&cgreq.proto_file);
+    let package_to_file_descriptor_map = cgreq
+        .proto_file
+        .into_iter()
+        .map(|file| (file.package.clone().unwrap_or_default(), file))
+        .collect::<HashMap<_, _>>();
+    // merge 2 hashmaps
+    let mut package_to_variables = HashMap::<String, Variables>::new();
+    for (package, subpackages) in package_to_subpackage_map {
+        package_to_variables
+            .entry(package)
+            .or_insert_with(Default::default)
+            .subpackages = subpackages;
+    }
+    for (package, file) in package_to_file_descriptor_map {
+        package_to_variables
+            .entry(package)
+            .or_insert_with(Default::default)
+            .file = Some(file);
+    }
 
-    let t = wrappers::ModRs { hogee: "foo" };
-    mod_rs.content = Some(t.render().unwrap());
+    for (package, variables) in package_to_variables {
+        let filename = package_to_filename(&package);
+        let contents = variables.render().unwrap();
 
-    cgres.file.push(mod_rs);
+        let mut output_file = <File as Default>::default();
+        output_file.name = Some(filename);
+        output_file.content = Some(contents);
+        cgres.file.push(output_file);
+    }
+
     cgres.serialize(&mut stdout())?;
     Ok(())
 }
--- puroro-plugin/src/wrappers.rs
+++ puroro-plugin/src/wrappers.rs
@@ -1,7 +1,2 @@
 use askama::Template;
 
-#[derive(Template)]
-#[template(path = "mod.rs.txt")]
-pub struct ModRs {
-    pub hogee: &'static str,
-}
