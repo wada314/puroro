b54d6a9f5d5078fa6b84318ca83cbb659f59fd0d
Shohei Wada <pc@wada314.jp>
2021-06-27 12:12:11 +0900

    generate cc sample proto for reference

--- update-plugin-protos.bat
+++ update-plugin-protos.bat
@@ -1,5 +1,5 @@
 cargo build -p puroro-plugin && ^
-$env:PURORO_PROTOC_PATH ^
+& $env:PURORO_PROTOC_PATH ^
     protobuf/src/google/protobuf/compiler/plugin.proto ^
     --plugin=protoc-gen-rust=./target/debug/puroro-plugin.exe ^
     --rust_out=./puroro-plugin/src/protos/ ^
