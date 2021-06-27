14be581defb470a2279efdc53162afd471282db8
Shohei Wada <pc@wada314.jp>
2021-06-28 00:40:27 +0900

    modify util interface

--- puroro-plugin/src/wrappers.rs
+++ puroro-plugin/src/wrappers.rs
@@ -1,5 +1,6 @@
 use crate::utils;
 use ::itertools::Itertools;
+use ::std::borrow::Borrow;
 use ::std::iter;
 
 #[derive(Debug, Clone)]
@@ -48,7 +49,10 @@ impl Enum {
     pub fn rust_absolute_path(&self) -> String {
         format!(
             "{path}::{ident}",
-            path = make_module_path(&self.proto_package, &self.proto_outer_messages),
+            path = make_module_path(
+                self.proto_package.iter().map(|s| s.borrow()),
+                self.proto_outer_messages.iter().map(|s| s.borrow())
+            ),
             ident = self.rust_ident
         )
     }
@@ -56,43 +60,41 @@ impl Enum {
 
 #[test]
 fn test_make_module_path() {
-    let package = "google.protobuf.compiler"
-        .split('.')
-        .map(|p| p.to_string())
-        .collect::<Vec<_>>();
-    let outer_messages = vec!["CodeGeneratorResponse".to_string()];
-    let empty: Vec<String> = Vec::new();
+    let package = "google.protobuf.compiler".split('.');
+    let outer_messages = iter::once("CodeGeneratorResponse");
+    let empty = iter::empty();
     assert_eq!(
         "self::puroro_root::google::protobuf::compiler",
-        make_module_path(&package, &empty)
+        make_module_path(package.clone(), empty.clone())
     );
     assert_eq!(
         "self::puroro_root::puroro_nested::code_generator_response",
-        make_module_path(&empty, &outer_messages)
+        make_module_path(empty.clone(), outer_messages.clone())
     );
     assert_eq!(
         "self::puroro_root::google::protobuf::compiler::puroro_nested::code_generator_response",
-        make_module_path(&package, &outer_messages)
+        make_module_path(package.clone(), outer_messages.clone())
     );
 }
 
-fn make_module_path(package: &Vec<String>, outer_messages: &Vec<String>) -> String {
-    let package_iter = package
-        .into_iter()
-        .map(|s| utils::get_keyword_safe_ident(s));
-    let outer_messages_iter = outer_messages
-        .into_iter()
-        .map(|s| utils::get_keyword_safe_ident(&utils::to_lower_snake_case(s)));
-    let maybe_puroro_nested = if outer_messages.is_empty() {
+fn make_module_path<'a, I, J>(package: I, outer_messages: J) -> String
+where
+    I: Iterator<Item = &'a str>,
+    J: Iterator<Item = &'a str> + Clone,
+{
+    let package = package.map(|s| utils::get_keyword_safe_ident(s));
+    let maybe_puroro_nested = if outer_messages.clone().count() == 0 {
         None
     } else {
         Some("puroro_nested".to_string())
     }
     .into_iter();
+    let outer_messages =
+        outer_messages.map(|s| utils::get_keyword_safe_ident(&utils::to_lower_snake_case(s)));
     let mut modules_iter = iter::once("self".to_string())
         .chain(iter::once("puroro_root".to_string()))
-        .chain(package_iter)
+        .chain(package)
         .chain(maybe_puroro_nested)
-        .chain(outer_messages_iter);
+        .chain(outer_messages);
     modules_iter.join("::")
 }
