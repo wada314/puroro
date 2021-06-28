44bbdc1ba6d53013f90f4b6ff03505145b3958da
Shohei Wada <pc@wada314.jp>
2021-06-28 17:30:00 +0900

    delete old files

--- puroro/src/lib.rs
+++ puroro/src/lib.rs
@@ -3,50 +3,14 @@
 #![cfg_attr(feature = "puroro-nightly", feature(min_type_alias_impl_trait))]
 #![allow(incomplete_features)]
 
-pub mod apply;
-mod collections;
 mod error;
-mod pow;
+
 pub mod tags;
 
 pub use error::{ErrorKind, PuroroError};
 pub type Result<T> = std::result::Result<T, PuroroError>;
-pub use collections::RepeatedField;
-pub use pow::Pow;
 
 // Re-exports
 #[cfg(feature = "puroro-bumpalo")]
 pub use ::bumpalo;
 pub use ::hashbrown;
-
-pub trait DeserializableFromIter {
-    fn merge_from_iter<I>(&mut self, iter: &mut I) -> Result<()>
-    where
-        I: Iterator<Item = ::std::io::Result<u8>>;
-}
-pub trait DeserializableFromSlice<'slice>: Sized {
-    fn deser_from_slice(slice: &'slice [u8]) -> Result<Self>;
-}
-
-pub trait Serializable: Sized {
-    fn serialize<W: std::io::Write>(&self, write: &mut W) -> Result<()>;
-}
-
-pub trait Message {
-    type InternalData: InternalData;
-    fn puroro_internal_data(&self) -> &Self::InternalData;
-
-    type BoxedType: AsMut<Self>;
-    fn into_boxed(self) -> Self::BoxedType;
-
-    type ImplTypeTag: tags::ImplTypeTag;
-}
-pub trait Enum {}
-
-pub trait MessageTag {}
-pub trait IsMessageImplOfTag<Tag> {}
-
-pub trait InternalData {
-    #[cfg(feature = "puroro-bumpalo")]
-    fn bumpalo(&self) -> &::bumpalo::Bump;
-}
