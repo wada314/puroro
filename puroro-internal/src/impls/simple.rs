b654392adf6e05ea0d11b8de94286dd3d355f34f
Shohei Wada <pc@wada314.jp>
2021-07-02 18:12:30 +0900

    move simple's type_gen

--- puroro-internal/src/impls/simple.rs
+++ puroro-internal/src/impls/simple.rs
@@ -1,95 +1,22 @@
-use std::borrow::Cow;
+mod type_gen;
 
-use ::puroro::{tags, FieldTypeGen, StructInternalTypeGen};
+use ::puroro::de::from_iter::ImplIsDeserializableFromIter;
+use ::puroro::{tags, StructInternalTypeGen};
 
 pub struct SimpleImpl;
 impl tags::ImplTypeTag for SimpleImpl {}
 
-// For variant types
-impl<L, S, V> FieldTypeGen<(L, (S, tags::wire::Variant<V>))> for SimpleImpl
-where
-    (S, tags::wire::Variant<V>): tags::NumericalFieldTypeTag,
-    <
-        (S, tags::wire::Variant<V>) as tags::NumericalFieldTypeTag
-    >::NativeType: LabelWrappedType<L>,
-{
-    type Type = <
-        <(S, tags::wire::Variant<V>) as tags::NumericalFieldTypeTag>::NativeType 
-            as LabelWrappedType<L>
-    >::Type;
-}
-// For bits32 types
-impl<L, S, V> FieldTypeGen<(L, (S, tags::wire::Bits32<V>))> for SimpleImpl
-where
-    (S, tags::wire::Bits32<V>): tags::NumericalFieldTypeTag,
-    <
-        (S, tags::wire::Bits32<V>) as tags::NumericalFieldTypeTag
-    >::NativeType: LabelWrappedType<L>,
-{
-    type Type = <
-        <(S, tags::wire::Bits32<V>) as tags::NumericalFieldTypeTag>::NativeType 
-            as LabelWrappedType<L>
-    >::Type;
-}
-// For bits64 types
-impl<L, S, V> FieldTypeGen<(L, (S, tags::wire::Bits64<V>))> for SimpleImpl
-where
-    (S, tags::wire::Bits64<V>): tags::NumericalFieldTypeTag,
-    <
-        (S, tags::wire::Bits64<V>) as tags::NumericalFieldTypeTag
-    >::NativeType: LabelWrappedType<L>,
-{
-    type Type = <
-        <(S, tags::wire::Bits64<V>) as tags::NumericalFieldTypeTag>::NativeType 
-            as LabelWrappedType<L>
-    >::Type;
-}
-
-// For length delimited types
-
-impl FieldTypeGen<(tags::Required, (tags::Proto2, tags::Bytes))> for SimpleImpl {
-    type Type = Cow<'static, [u8]>;
-}
-impl FieldTypeGen<(tags::Optional, (tags::Proto2, tags::Bytes))> for SimpleImpl {
-    type Type = Cow<'static, [u8]>;
-}
-impl FieldTypeGen<(tags::Unlabeled, (tags::Proto3, tags::Bytes))> for SimpleImpl {
-    type Type = Vec<u8>;
-}
-impl FieldTypeGen<(tags::Optional, (tags::Proto3, tags::Bytes))> for SimpleImpl {
-    type Type = Option<Vec<u8>>;
-}
-impl<S> FieldTypeGen<(tags::Repeated, (S, tags::Bytes))> for SimpleImpl {
-    type Type = Vec<Vec<u8>>;
-}
-
-impl FieldTypeGen<(tags::Required, (tags::Proto2, tags::String))> for SimpleImpl {
-    type Type = Cow<'static, str>;
-}
-impl FieldTypeGen<(tags::Optional, (tags::Proto2, tags::String))> for SimpleImpl {
-    type Type = Cow<'static, str>;
-}
-impl FieldTypeGen<(tags::Unlabeled, (tags::Proto3, tags::String))> for SimpleImpl {
-    type Type = String;
-}
-impl FieldTypeGen<(tags::Optional, (tags::Proto3, tags::String))> for SimpleImpl {
-    type Type = Option<String>;
-}
-impl<S> FieldTypeGen<(tags::Repeated, (S, tags::String))> for SimpleImpl {
-    type Type = Vec<String>;
-}
-
-impl<S, M> FieldTypeGen<(tags::Required, (S, tags::Message<M>))> for SimpleImpl {
-    type Type = Option<Box<M>>;
-}
-impl<S, M> FieldTypeGen<(tags::Optional, (S, tags::Message<M>))> for SimpleImpl {
-    type Type = Option<Box<M>>;
-}
-impl<S, M> FieldTypeGen<(tags::Unlabeled, (S, tags::Message<M>))> for SimpleImpl {
-    type Type = Option<Box<M>>;
-}
-impl<S, M> FieldTypeGen<(tags::Repeated, (S, tags::Message<M>))> for SimpleImpl {
-    type Type = Vec<M>;
+impl ImplIsDeserializableFromIter for SimpleImpl {
+    fn deser_field<LabelAndType, I>(
+        field: &mut <Self as puroro::FieldTypeGen<LabelAndType>>::Type,
+        input: puroro::de::FieldData<I>,
+    ) -> puroro::Result<()>
+    where
+        I: Iterator<Item = std::io::Result<u8>>,
+        Self: puroro::FieldTypeGen<LabelAndType>,
+    {
+        todo!()
+    }
 }
 
 // Struct's internal type generator
@@ -97,22 +24,4 @@ impl<S, M> FieldTypeGen<(tags::Repeated, (S, tags::Message<M>))> for SimpleImpl 
 impl StructInternalTypeGen for SimpleImpl {
     // TODO
     type Type = ();
-}
-
-// Utilities
-pub trait LabelWrappedType<L> {
-    type Type;
-}
-impl<T> LabelWrappedType<tags::Required> for T {
-    // TODO: Revisit... T or Option<T>
-    type Type = Option<T>;
-}
-impl<T> LabelWrappedType<tags::Optional> for T {
-    type Type = Option<T>;
-}
-impl<T> LabelWrappedType<tags::Unlabeled> for T {
-    type Type = T;
-}
-impl<T> LabelWrappedType<tags::Repeated> for T {
-    type Type = Vec<T>;
 }
