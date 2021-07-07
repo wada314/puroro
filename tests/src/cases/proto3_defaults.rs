ecc4b8936bdec6b1f0bf2df2bc322b976045a3b8
Shohei Wada <pc@wada314.jp>
2021-07-08 00:19:16 +0900

    adding default tests

--- tests/src/cases/proto3_defaults.rs
+++ tests/src/cases/proto3_defaults.rs
@@ -1,15 +1,30 @@
 /// About the proto3's default values, check the following official document:
 /// https://github.com/protocolbuffers/protobuf/blob/master/docs/field_presence.md
-use crate::tests_pb::proto3_defaults::Msg;
+use crate::tests_pb::proto3_defaults::{Msg, Submsg};
 use ::puroro::DeserFromBytesIter;
 
-const INPUT_FIELD1_I32_ZERO: &[u8] = &[0x08, 0x00];
-const INPUT_FIELD1_I32_ONE: &[u8] = &[0x08, 0x01];
-const INPUT_FIELD4_F32_ZERO: &[u8] = &[0x25, 0x00, 0x00, 0x00, 0x00];
-const INPUT_FIELD4_F32_ONE: &[u8] = &[0x25, 0x00, 0x00, 0x80, 0x3F]; // 0x3F80_0000 == 1.0f32
-const INPUT_FIELD3_I32_ZERO: &[u8] = &[0x18, 0x00];
-const INPUT_FIELD3_I32_ONE: &[u8] = &[0x18, 0x01];
-const INPUT_FIELD3_I32_PACKED_ZERO_TO_THREE: &[u8] = &[0x1A, 0x04, 0x00, 0x01, 0x02, 0x03];
+const INPUT_FIELD1_I32_ZERO: &[u8] = &[(1 << 3) | 0, 0x00];
+const INPUT_FIELD1_I32_ONE: &[u8] = &[(1 << 3) | 0, 0x01];
+const INPUT_FIELD2_I32_ZERO: &[u8] = &[(2 << 3) | 0, 0x00];
+const INPUT_FIELD2_I32_ONE: &[u8] = &[(2 << 3) | 0, 0x01];
+const INPUT_FIELD3_I32_ZERO: &[u8] = &[(3 << 3) | 0, 0x00];
+const INPUT_FIELD3_I32_ONE: &[u8] = &[(3 << 3) | 0, 0x01];
+const INPUT_FIELD3_I32_PACKED_ZERO_TO_THREE: &[u8] = &[(3 << 3) | 2, 0x04, 0x00, 0x01, 0x02, 0x03];
+const INPUT_FIELD4_F32_ZERO: &[u8] = &[(4 << 3) | 5, 0x00, 0x00, 0x00, 0x00];
+const INPUT_FIELD4_F32_ONE: &[u8] = &[(4 << 3) | 5, 0x00, 0x00, 0x80, 0x3F]; // 0x3F80_0000 == 1.0f32
+const INPUT_FIELD5_STRING_EMPTY: &[u8] = &[(5 << 3) | 0x02, 0x00];
+const INPUT_FIELD5_STRING_TEST2: &[u8] = &[
+    (5 << 3) | 0x02,
+    0x05,
+    't' as u8,
+    'e' as u8,
+    's' as u8,
+    't' as u8,
+    '2' as u8,
+];
+const INPUT_FIELDS6_MSG_EMPTY: &[u8] = &[(6 << 3) | 0x02, 0x00];
+const INPUT_FIELDS6_MSG_FIELD1_I32_ZERO: &[u8] = &[(6 << 3) | 0x02, 0x02, (1 << 3) | 0, 0x00];
+const INPUT_FIELDS6_MSG_FIELD1_I32_ONE: &[u8] = &[(6 << 3) | 0x02, 0x02, (1 << 3) | 0, 0x01];
 
 #[test]
 fn test_i32_scalar() {
@@ -28,25 +43,27 @@ fn test_i32_scalar() {
 }
 
 #[test]
-fn test_f32_scalar() {
+fn test_i32_optional() {
     use std::io::Read as _;
     let mut msg = <Msg as Default>::default();
-    msg.f32_scalar = 10.0;
-    assert_eq!(10.0, msg.f32_scalar);
+    assert_eq!(None, msg.i32_optional);
 
-    // Deser 0 into the field, but it is a default value so it should be ignored
-    msg.deser(INPUT_FIELD4_F32_ZERO.bytes()).unwrap();
-    assert_eq!(10.0, msg.f32_scalar);
+    msg.i32_optional = Some(10);
+    assert_eq!(Some(10), msg.i32_optional);
 
-    // Deser 1 into the field. Should overwrite the value.
-    msg.deser(INPUT_FIELD4_F32_ONE.bytes()).unwrap();
-    assert_eq!(1.0, msg.f32_scalar);
+    msg.deser(INPUT_FIELD2_I32_ZERO.bytes()).unwrap();
+    assert_eq!(Some(0), msg.i32_optional);
+
+    msg.deser(INPUT_FIELD2_I32_ONE.bytes()).unwrap();
+    assert_eq!(Some(1), msg.i32_optional);
 }
 
 #[test]
 fn test_i32_repeated() {
     use std::io::Read as _;
     let mut msg = <Msg as Default>::default();
+    assert!(msg.i32_repeated.is_empty());
+
     msg.i32_repeated = vec![10, 20];
     assert_eq!(&vec![10, 20], &msg.i32_repeated);
 
@@ -59,4 +76,42 @@ fn test_i32_repeated() {
     msg.deser(INPUT_FIELD3_I32_PACKED_ZERO_TO_THREE.bytes())
         .unwrap();
     assert_eq!(&vec![10, 20, 0, 1, 0, 1, 2, 3], &msg.i32_repeated);
+}
+
+#[test]
+fn test_string_scalar() {
+    use std::io::Read as _;
+    let mut msg = <Msg as Default>::default();
+    assert_eq!("", msg.string_scalar);
+
+    msg.string_scalar = "test1".to_string();
+    assert_eq!("test1", msg.string_scalar);
+
+    msg.deser(INPUT_FIELD5_STRING_EMPTY.bytes()).unwrap();
+    assert_eq!("test1", msg.string_scalar);
+
+    msg.deser(INPUT_FIELD5_STRING_TEST2.bytes()).unwrap();
+    assert_eq!("test2", msg.string_scalar);
+}
+
+#[test]
+fn test_message_scalar() {
+    use std::io::Read as _;
+    let mut msg = <Msg as Default>::default();
+    assert!(msg.submsg_scalar.is_none());
+
+    msg.submsg_scalar = Some(Box::new(<Submsg as Default>::default()));
+    assert!(msg.submsg_scalar.is_some());
+    assert_eq!(0, msg.submsg_scalar.as_ref().unwrap().i32_scalar);
+    msg.submsg_scalar.as_mut().unwrap().i32_scalar = 10;
+    assert_eq!(10, msg.submsg_scalar.as_ref().unwrap().i32_scalar);
+
+    msg.deser(INPUT_FIELDS6_MSG_FIELD1_I32_ZERO.bytes())
+        .unwrap();
+    assert!(msg.submsg_scalar.is_some());
+    assert_eq!(10, msg.submsg_scalar.as_ref().unwrap().i32_scalar);
+
+    msg.deser(INPUT_FIELDS6_MSG_FIELD1_I32_ONE.bytes()).unwrap();
+    assert!(msg.submsg_scalar.as_ref().is_some());
+    assert_eq!(1, msg.submsg_scalar.unwrap().i32_scalar);
 }
