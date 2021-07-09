862b7e75629a47e906c4ca1b89bcd8294b818dce
Shohei Wada <pc@wada314.jp>
2021-07-09 16:29:41 +0900

    generating se

--- puroro-internal/src/se.rs
+++ puroro-internal/src/se.rs
@@ -1,15 +1,20 @@
 pub mod to_io_write;
 use crate::{FieldTypeGen, Result, StructInternalTypeGen};
 
-pub fn ser_to_io_write<Msg, W>(message: &Msg, out: &mut W) -> Result<()> {
-    todo!()
-}
-
 pub trait SerFieldToIoWrite<LabelAndType>:
     FieldTypeGen<LabelAndType> + StructInternalTypeGen
 {
     fn ser_to_io_write<W>(
         field: &<Self as FieldTypeGen<LabelAndType>>::Type,
+        out: &mut W,
+        internal_data: &<Self as StructInternalTypeGen>::Type,
+    ) -> Result<()>
+    where
+        W: ::std::io::Write;
+}
+
+pub trait SerInternalDataToIoWrite: StructInternalTypeGen {
+    fn ser_to_io_write<W>(
         out: &mut W,
         internal_data: &<Self as StructInternalTypeGen>::Type,
     ) -> Result<()>
