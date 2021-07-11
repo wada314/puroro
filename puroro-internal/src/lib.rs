5a1ef3b80a1c55608dcd56e27c064647318ccb47
Shohei Wada <pc@wada314.jp>
2021-07-12 03:49:47 +0900

    Add self recursive example, which is not working!!

--- puroro-internal/src/lib.rs
+++ puroro-internal/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(incomplete_features)]
+#![feature(generic_associated_types)]
 pub mod de;
 pub mod impls;
 pub mod se;
--- tests/src/cases.rs
+++ tests/src/cases.rs
@@ -1,3 +1,4 @@
 mod official_samples;
 mod proto3_defaults;
+mod self_recursive;
 mod ser_tests;
