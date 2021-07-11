afe6965c69dde52064564a6fa93e8653545bfb9a
Shohei Wada <pc@wada314.jp>
2021-07-11 17:00:24 +0900

    using wrong proto

--- tests/src/cases/ser_tests.rs
+++ tests/src/cases/ser_tests.rs
@@ -1,4 +1,4 @@
-use crate::tests_pb::proto3_defaults::{Msg, Submsg};
+use crate::tests_pb::ser_tests::{msg::Submsg, Msg};
 use ::puroro::SerToIoWrite;
 
 #[test]
@@ -16,4 +16,13 @@ fn test_i32_unlabeled() {
     msg.i32_unlabeled = 10;
     msg.ser(&mut buf).unwrap();
     assert_eq!(&[(1 << 3) | 0, 10], buf.as_slice());
+}
+
+#[test]
+fn test_submsg_unlabeled() {
+    let mut msg = Msg::default();
+    let mut buf: Vec<u8> = Vec::new();
+    msg.submsg_unlabeled = Some(Box::new(Submsg::default()));
+    msg.ser(&mut buf).unwrap();
+    assert_eq!(&[(7 << 3) | 2, 0], buf.as_slice());
 }
