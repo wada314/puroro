d92a27e365e6544e20ebc48d36a8bda05042b0d6
Shohei Wada <pc@wada314.jp>
2021-07-12 00:35:08 +0900

    update the update script

--- update-plugin-protos.bat
+++ update-plugin-protos.bat
@@ -1,5 +1,7 @@
 cargo build -p puroro-plugin && ^
-& $env:PURORO_PROTOC_PATH ^
+rmdir .\puroro-plugin\src\protos /S /Q && ^
+mkdir .\puroro-plugin\src\protos
+call %PURORO_PROTOC_PATH% ^
     protobuf/src/google/protobuf/compiler/plugin.proto ^
     --plugin=protoc-gen-rust=./target/debug/puroro-plugin.exe ^
     --rust_out=./puroro-plugin/src/protos/ ^
