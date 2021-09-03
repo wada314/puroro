04eec00a2bb0ceb8c4f4ab98855cc30f764fc4b5
Shohei Wada <pc@wada314.jp>
2021-09-03 21:39:35 +0900

    merged repeated field menndokusai...

--- puroro-internal/src/impls/merged.rs
+++ puroro-internal/src/impls/merged.rs
@@ -1,5 +1,6 @@
-use ::puroro::RepeatedField;
+use ::puroro::{Either, RepeatedField};
 use ::std::iter::Chain;
+use ::std::ops::Deref;
 
 pub struct MergedRepeatedField<T, U>(T, U);
 impl<T, U> MergedRepeatedField<T, U> {
@@ -16,5 +17,25 @@ where
     type IntoIter = Chain<<T as IntoIterator>::IntoIter, <U as IntoIterator>::IntoIter>;
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter().chain(self.1.into_iter())
+    }
+}
+
+pub struct MergedRepeatedLDField<T, U>(T, U);
+impl<T, U> MergedRepeatedLDField<T, U> {
+    pub fn new(t: T, u: U) -> Self {
+        Self(t, u)
+    }
+}
+impl<'msg, T, U> IntoIterator for MergedRepeatedLDField<T, U>
+where
+    T: RepeatedField<'msg> + IntoIterator,
+    U: RepeatedField<'msg> + IntoIterator,
+    <T as IntoIterator>::Item: Deref<Target = <<U as IntoIterator>::Item as Deref>::Target>,
+    <U as IntoIterator>::Item: Deref,
+{
+    type Item = Either<<T as IntoIterator>::Item, <U as IntoIterator>::Item>;
+    type IntoIter = Chain<<T as IntoIterator>::IntoIter, <U as IntoIterator>::IntoIter>; // TODO!!
+    fn into_iter(self) -> Self::IntoIter {
+        todo!()
     }
 }
