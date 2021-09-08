cf6720bbdcc86632a4d6de64b1fe1da9dfcb6f89
Shohei Wada <pc@wada314.jp>
2021-09-09 08:01:37 +0900

    hoge

--- tests/src/cases/oneof.rs
+++ tests/src/cases/oneof.rs
@@ -1,3 +1,4 @@
+use ::std::borrow::Cow;
 use ::tests_pb::oneofs2::msg::{
     GroupOne as GroupOne2, GroupThree as GroupThree2, GroupTwo as GroupTwo2,
 };
@@ -13,12 +14,30 @@ fn test_oneof_simple2() {
     assert!(msg.group_one().is_none());
     assert!(msg.group_two().is_none());
     assert!(msg.group_three().is_none());
+    assert!(msg.g1_int32().is_none());
+    assert!(msg.g1_string().is_none());
+    assert!(msg.g2_f32().is_none());
+    assert!(msg.g2_string().is_none());
+    assert!(msg.g2_submsg().is_none());
+    assert!(msg.g3_int32().is_none());
+
     msg.group_one = Some(GroupOne2::G1Int32(100));
     assert!(matches!(msg.group_one(), Some(GroupOne2::G1Int32(100))));
     assert!(matches!(msg.g1_int32(), Some(100)));
     assert!(msg.g1_string().is_none());
-    msg.group_one = Some(GroupOne2::G1String("Test".into()));
+    msg.group_one = Some(GroupOne2::G1String(Cow::Borrowed("Test")));
     assert!(matches!(msg.group_one(), Some(GroupOne2::G1String("Test"))));
     assert!(matches!(msg.g1_string(), Some("Test")));
     assert!(msg.g1_int32().is_none());
+
+    msg.group_two = Some(GroupTwo2::G2F32(100.0));
+    assert_eq!(msg.group_two(), Some(GroupTwo2::G2F32(100.0)));
+    assert_eq!(msg.g2_f32(), Some(100.0));
+    assert!(msg.g2_string().is_none());
+    assert!(msg.g2_submsg().is_none());
+    msg.group_two = Some(GroupTwo2::G2String(Cow::Borrowed("Test")));
+    assert_eq!(msg.group_two(), Some(GroupTwo2::G2String("Test")));
+    assert_eq!(msg.g2_string(), Some("Test"));
+    assert!(msg.g2_f32().is_none());
+    assert!(msg.g2_submsg().is_none());
 }
