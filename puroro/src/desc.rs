62121f3231ef1724868cbba293e37db0f8168e27
Shohei Wada <pc@wada314.jp>
2021-09-09 19:05:14 +0900

    desc...

--- puroro/src/desc.rs
+++ puroro/src/desc.rs
@@ -1,2 +1,19 @@
-struct MessageDescriptor;
-struct FieldDescriptor;
+use ::once_cell::sync::Lazy;
+
+struct MessageDescriptor {
+    lazy_fields: Lazy<&'static [FieldDescriptor]>,
+}
+impl MessageDescriptor {
+    pub fn fields(&self) -> &'static [FieldDescriptor] {
+        *Lazy::force(&self.lazy_fields)
+    }
+}
+
+struct FieldDescriptor {
+    lazy_containing_type: Lazy<&'static MessageDescriptor>,
+}
+impl FieldDescriptor {
+    pub fn containing_type(&self) -> &MessageDescriptor {
+        *Lazy::force(&self.lazy_containing_type)
+    }
+}
