fdffb0d85423fff99405bca0b897a0d21ee0c503
Shohei Wada <pc@wada314.jp>
2021-09-16 00:17:05 +0900

    adding merged test. Need Optional<M> impl??

--- tests/src/cases/merged.rs
+++ tests/src/cases/merged.rs
@@ -1,4 +1,5 @@
 use ::itertools::Itertools;
+use ::tests_pb::full_coverage3::msg::{Submsg, SubmsgTrait as _};
 use ::tests_pb::full_coverage3::{Msg, MsgTrait as _};
 use ::tests_pb::oneofs3::{Msg as OneofMsg, MsgTrait as _};
 
@@ -71,5 +72,24 @@ fn test_get_i32_repeated_field() {
     assert_eq!(
         vec![1, 3, 5],
         (&msg_1, &msg_3_5).i32_repeated().into_iter().collect_vec()
+    );
+}
+
+#[test]
+fn test_get_msg_optional_field() {
+    let submsg_3 = Submsg { i32_unlabeled: 3 };
+    let none = Msg {
+        submsg_optional: None,
+        ..Default::default()
+    };
+    let msg_3 = Msg {
+        submsg_optional: Some(Box::new(submsg_3.clone())),
+        ..Default::default()
+    };
+    assert_eq!(None, (&none, &none).submsg_optional());
+    assert!((&msg_3, &none).submsg_optional().is_some());
+    assert_eq!(
+        3,
+        (&msg_3, &none).submsg_optional().unwrap().i32_unlabeled()
     );
 }
