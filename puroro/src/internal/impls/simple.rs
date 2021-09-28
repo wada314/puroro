db7b29ebe1a9ffc7ea6f5460367fff3bbd4bc21e
Shohei Wada <pc@wada314.jp>
2021-09-28 12:51:17 +0900

    doc...

--- puroro/src/internal/impls/simple.rs
+++ puroro/src/internal/impls/simple.rs
@@ -1,5 +1,7 @@
 //! A very basic implementation of protobuf message in generated Rust code.
 //!
+//! # Basic
+//!
 //! puroro generates a plain struct which has public fields.
 //! For an input .proto file like this:
 //! ```protobuf
@@ -30,6 +32,84 @@
 //! | `string`           | `Option<String>`        | `String`    | `Vec<String>`|
 //! | `SomeMessage`      | `Option<Box<SomeMessage>>`|`Option<Box<SomeMessage>>`|`Vec<SomeMessage>`|
 //!
+//! ## Enums
+//!
+//! ### proto2
+//!
+//! ```protobuf
+//! syntax = "proto2";
+//! enum MyEnum {
+//!     ZERO = 0;
+//!     TWENTY_ONE = 21;
+//! }
+//! ```
+//!
+//! Generates an enum like this:
+//!
+//! ```ignore
+//! enum MyEnum {
+//!     Zero,
+//!     TwentyOne,
+//! }
+//! impl puroro::Enum2 for MyEnum { /* ... */ }
+//! ```
+//!
+//! Please note that the integral values (`0`, `21`) are not directly
+//! assigned to the Rust enum values.
+//! Though the trait [`Enum2`](crate::Enum2) has supertraits
+//! [`TryFrom<i32, Error=i32>`](`std::convert::TryFrom`) and [`Into<i32>`].
+//! So you can convert the enum value from / to i32 as follows:
+//!
+//! ```rust
+//! # #[derive(Debug, PartialEq)]
+//! # enum MyEnum {
+//! #     Zero,
+//! #     TwentyOne,
+//! # }
+//! # impl std::convert::TryFrom<i32> for MyEnum {
+//! #     type Error = i32;
+//! #     fn try_from(val: i32) -> Result<Self, Self::Error> {
+//! #         match val.clone() {
+//! #             0 => Ok(MyEnum::Zero),
+//! #             21 => Ok(MyEnum::TwentyOne),
+//! #             _ => Err(val),
+//! #         }
+//! #     }
+//! # }
+//! # impl Into<i32> for MyEnum {
+//! #     fn into(self) -> i32 {
+//! #         match self {
+//! #             MyEnum::Zero => 0,
+//! #             MyEnum::TwentyOne => 21,
+//! #         }
+//! #     }
+//! # }
+//! # use std::convert::TryFrom;
+//! assert_eq!(MyEnum::TwentyOne, MyEnum::try_from(21).unwrap());
+//! assert_eq!(21, MyEnum::TwentyOne.into());
+//! ```
+//!
+//! ### proto3
+//!
+//! ```protobuf
+//! syntax = "proto3";
+//! enum MyEnum {
+//!     ZERO = 0;
+//!     TWENTY_ONE = 21;
+//! }
+//! ```
+//!
+//! # Nested message / enum
+//!
+//! ```protobuf
+//! syntax = "proto3";
+//! message MyMessage {
+//!     message MySubMessage {
+//!         int32 my_number = 1;
+//!     }
+//!     MySubMessage my_sub_message = 1;
+//! }
+//! ```
 //!
 
 pub mod de;
