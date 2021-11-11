2e092baadd13ed202f37f4fe9a69d2c288ba7146
Shohei Wada <pc@wada314.jp>
2021-11-11 09:22:03 +0900

    bumpalo owned new

--- puroro/src/common_types.rs
+++ puroro/src/common_types.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::bumpalo::Bump;
+use crate::internal::impls::bumpalo::BumpaloDefault;
 use ::std::fmt::Debug;
 use ::std::ops::{Deref, DerefMut};
 
@@ -23,6 +25,18 @@ pub fn merge<T, U>(t: T, u: U) -> Merged<T, U> {
 impl<T: Clone, U: Clone> Clone for Merged<T, U> {
     fn clone(&self) -> Self {
         Self(self.0.clone(), self.1.clone())
+    }
+}
+impl<T> Merged<T, Box<Bump>>
+where
+    T: BumpaloDefault<'static>,
+{
+    pub fn new() -> Self {
+        let boxed_bump = Box::new(Bump::new());
+        merge(
+            T::default_in(unsafe { ::std::mem::transmute(boxed_bump.as_ref()) }),
+            boxed_bump,
+        )
     }
 }
 
