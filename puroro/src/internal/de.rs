2d82c6f538c87122e92d5815c16412b826837191
Shohei Wada <pc@wada314.jp>
2021-11-11 10:12:43 +0900

    macro-nize deser message proxy

--- puroro/src/internal/de.rs
+++ puroro/src/internal/de.rs
@@ -16,7 +16,8 @@ pub mod from_iter;
 
 use self::from_iter::ScopedIter;
 use crate::internal::types::FieldData;
-use crate::Result;
+use crate::{EmptyMessageWrapper, Merged, Result};
+use ::std::ops::DerefMut;
 
 pub trait DeserMessageFromBytesIter {
     fn deser_field<I>(
@@ -27,64 +28,65 @@ pub trait DeserMessageFromBytesIter {
     where
         I: Iterator<Item = ::std::io::Result<u8>>;
 }
+macro_rules! impl_deser_field {
+    ($lambda:expr) => {
+        fn deser_field<I>(
+            &mut self,
+            field_number: i32,
+            data: FieldData<&mut ScopedIter<I>>,
+        ) -> Result<()>
+        where
+            I: Iterator<Item = std::io::Result<u8>>,
+        {
+            ($lambda)(self).deser_field(field_number, data)
+        }
+    };
+}
 impl<T> DeserMessageFromBytesIter for &'_ mut T
 where
     T: DeserMessageFromBytesIter,
 {
-    fn deser_field<I>(
-        &mut self,
-        field_number: i32,
-        data: FieldData<&mut ScopedIter<I>>,
-    ) -> Result<()>
-    where
-        I: Iterator<Item = std::io::Result<u8>>,
-    {
-        (*self).deser_field(field_number, data)
-    }
+    impl_deser_field!(DerefMut::deref_mut);
 }
 impl<T> DeserMessageFromBytesIter for Box<T>
 where
     T: DeserMessageFromBytesIter,
 {
-    fn deser_field<I>(
-        &mut self,
-        field_number: i32,
-        data: FieldData<&mut ScopedIter<I>>,
-    ) -> Result<()>
-    where
-        I: Iterator<Item = std::io::Result<u8>>,
-    {
-        Box::as_mut(self).deser_field(field_number, data)
-    }
+    impl_deser_field!(DerefMut::deref_mut);
 }
 impl<'bump, T> DeserMessageFromBytesIter for crate::bumpalo::boxed::Box<'bump, T>
 where
     T: DeserMessageFromBytesIter,
 {
-    fn deser_field<I>(
-        &mut self,
-        field_number: i32,
-        data: FieldData<&mut ScopedIter<I>>,
-    ) -> Result<()>
-    where
-        I: Iterator<Item = std::io::Result<u8>>,
-    {
-        crate::bumpalo::boxed::Box::as_mut(self).deser_field(field_number, data)
-    }
+    impl_deser_field!(DerefMut::deref_mut);
 }
 impl<T> DeserMessageFromBytesIter for Option<T>
 where
     T: DeserMessageFromBytesIter + Default,
 {
-    fn deser_field<I>(
-        &mut self,
-        field_number: i32,
-        data: FieldData<&mut ScopedIter<I>>,
-    ) -> Result<()>
-    where
-        I: Iterator<Item = std::io::Result<u8>>,
-    {
-        self.get_or_insert_with(Default::default)
-            .deser_field(field_number, data)
-    }
+    impl_deser_field!(|x| Option::get_or_insert_with(x, Default::default));
+}
+impl<T> DeserMessageFromBytesIter for Merged<T, ()>
+where
+    T: DeserMessageFromBytesIter,
+{
+    impl_deser_field!(Merged::first_mut);
+}
+impl<T> DeserMessageFromBytesIter for Merged<(), T>
+where
+    T: DeserMessageFromBytesIter,
+{
+    impl_deser_field!(Merged::last_mut);
+}
+impl<T, U> DeserMessageFromBytesIter for Merged<T, EmptyMessageWrapper<U>>
+where
+    T: DeserMessageFromBytesIter,
+{
+    impl_deser_field!(Merged::first_mut);
+}
+impl<T, U> DeserMessageFromBytesIter for Merged<EmptyMessageWrapper<T>, U>
+where
+    U: DeserMessageFromBytesIter,
+{
+    impl_deser_field!(Merged::last_mut);
 }
