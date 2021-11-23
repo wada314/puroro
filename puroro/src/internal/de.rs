30350ce0e34ff9bdd192a728aba33d5d89333ac8
Shohei Wada <pc@wada314.jp>
2021-11-23 12:32:53 +0900

    tests-pb compile passed, test failing...

--- puroro/src/internal/de.rs
+++ puroro/src/internal/de.rs
@@ -16,6 +16,7 @@ pub mod from_iter;
 
 use self::from_iter::ScopedIter;
 use crate::internal::types::FieldData;
+use crate::internal::NoAllocBumpBox;
 use crate::Result;
 use ::std::ops::DerefMut;
 
@@ -55,10 +56,10 @@ where
     where
         I: Iterator<Item = std::io::Result<u8>>,
     {
-        Box::as_mut(self).deser_field(field_number, data)
+        self.deref_mut().deser_field(field_number, data)
     }
 }
-impl<'bump, T> DeserMessageFromBytesIter for crate::bumpalo::boxed::Box<'bump, T>
+impl<'bump, T> DeserMessageFromBytesIter for NoAllocBumpBox<T>
 where
     T: DeserMessageFromBytesIter,
 {
@@ -70,7 +71,7 @@ where
     where
         I: Iterator<Item = std::io::Result<u8>>,
     {
-        crate::bumpalo::boxed::Box::as_mut(self).deser_field(field_number, data)
+        self.deref_mut().deser_field(field_number, data)
     }
 }
 impl<T> DeserMessageFromBytesIter for Option<T>
