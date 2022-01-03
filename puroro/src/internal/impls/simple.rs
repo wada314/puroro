1b10144b94a9ca5fad9d32be414ac5555e9aa576
Shohei Wada <pc@wada314.jp>
2022-01-03 22:33:26 +0900

    yeah

--- puroro/src/internal/impls/simple.rs
+++ puroro/src/internal/impls/simple.rs
@@ -67,5 +67,123 @@
 //!
 //! ## Fields
 //!
+//! ### proto2 & proto3 optional numeric fields
+//!
+//! For either of the following inputs:
+//!
+//! ```protobuf
+//! optional int32 foo = 1;
+//! required int32 foo = 1;
+//! ```
+//!
+//! puroro generates the methods like this:
+//!
+//! ```rust
+//! # pub struct Message;
+//! # impl Message {
+//! // Returns the field value if the field is present.
+//! // If the field is not present, then returns the "default" value,
+//! // which is normally 0 but in proto2 you can override it with
+//! // a field option like `[default = 10]`.
+//! pub fn foo(&self) -> i32 {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // Returns `Some` if the field is present, and `None` if not.
+//! // Unlike the `foo()` method, the default value setting does not
+//! // make any effect to this method.
+//! pub fn foo_opt(&self) -> Option<i32> {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // A shorthand of `self.foo_opt().is_some()`.
+//! pub fn has_foo(&self) -> bool {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // Returns a mutable reference. Similar to `Option::get_or_insert_with()`,
+//! // it sets the field present at invocation timing (even if the returned
+//! // mutable reference is not used).
+//! pub fn foo_mut(&mut self) -> &mut i32 {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // Unset the field.
+//! pub fn clear_foo(&mut self) {
+//! #   todo!()
+//!     // ...
+//! }
+//! # }
+//! ```
+//!
+//! A list of corresponding primitive types between
+//! protobuf and rust:
+//!
+//! |protobuf type|rust type|
+//! |-------------|---------|
+//! | `int32`    | `i32` |
+//! | `sint32`   | `i32` |
+//! | `sfixed32` | `i32` |
+//! | `uint32`   | `u32` |
+//! | `fixed32`  | `u32` |
+//! | `int64`    | `i64` |
+//! | `sint64`   | `i64` |
+//! | `sfixed64` | `i64` |
+//! | `uint64`   | `u64` |
+//! | `fixed64`  | `u64` |
+//! | `float`    | `f32` |
+//! | `double`   | `f64` |
+//! | `bool`     | `bool`|
+//!
+//! ### proto3 scalar numeric fields
+//!
+//! For the following input:
+//!
+//! ```protobuf
+//! int32 foo = 1;
+//! ```
+//!
+//! puroro generates the methods like this:
+//!
+//! ```rust
+//! # pub struct Message;
+//! # impl Message {
+//! // Returns the field value.
+//! pub fn foo(&self) -> i32 {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // If the field value is non-`0`, then returns `Some` wrapped value.
+//! // If the field value is `0`, then returns `None`.
+//! pub fn foo_opt(&self) -> Option<i32> {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // A shorthand of `self.foo_opt().is_some()`.
+//! pub fn has_foo(&self) -> bool {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // Returns a mutable reference.
+//! pub fn foo_mut(&mut self) -> &mut i32 {
+//! #   todo!()
+//!     // ...
+//! }
+//!
+//! // Set the field value to `0`.
+//! pub fn clear_foo(&mut self) {
+//! #   todo!()
+//!     // ...
+//! }
+//! # }
+//! ```
+//!
 
 pub mod de;
