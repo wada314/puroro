277bd29112ea192c102350021cb77fa834be2e6b
Shohei Wada <pc@wada314.jp>
2022-01-04 23:46:46 +0900

    hmm...

--- puroro/src/lib.rs
+++ puroro/src/lib.rs
@@ -89,3 +89,78 @@ impl<T> DerefMut for BumpaloOwned<T> {
 }
 
 impl<M, T> Message<M> for BumpaloOwned<T> where T: Message<M> {}
+
+/////////////////////////////////////
+
+#[derive(Clone)]
+struct FakeNum;
+impl From<FakeNum> for i32 {
+    fn from(_: FakeNum) -> i32 {
+        0
+    }
+}
+struct FakeStr;
+impl AsRef<str> for FakeStr {
+    fn as_ref(&self) -> &str {
+        ""
+    }
+}
+
+#[derive(Clone)]
+struct FakeMsg;
+impl MsgTrait for FakeMsg {
+    type Msg3Type<'a>
+    where
+        Self: 'a,
+    = FakeMsg;
+}
+impl<'a> MsgTrait for &'a FakeMsg {
+    type Msg3Type<'b>
+    where
+        Self: 'b,
+    = FakeMsg;
+}
+
+trait MsgTrait {
+    fn int_opt(&self) -> Option<i32> {
+        None
+    }
+    fn str_opt(&self) -> Option<&str> {
+        None
+    }
+    type Msg3Type<'a>: MsgTrait
+    where
+        Self: 'a;
+    fn msg_opt(&self) -> Option<Self::Msg3Type<'_>> {
+        None
+    }
+}
+
+struct Msg<F1, F2, F3, const N: usize>(F1, F2, F3);
+impl<F1, F2, F3, const N: usize> MsgTrait for Msg<F1, F2, F3, N>
+where
+    F1: Clone + Into<i32>,
+    F2: AsRef<str>,
+    for<'a> &'a F3: MsgTrait,
+{
+    fn int_opt(&self) -> Option<i32> {
+        if N == 1 {
+            Some(self.0.clone().into())
+        } else {
+            None
+        }
+    }
+
+    fn str_opt(&self) -> Option<&str> {
+        if N == 2 { Some(self.1.as_ref()) } else { None }
+    }
+
+    type Msg3Type<'a>
+    where
+        Self: 'a,
+    = &'a F3;
+
+    fn msg_opt(&self) -> Option<Self::Msg3Type<'_>> {
+        if N == 3 { Some(&self.2) } else { None }
+    }
+}
