3b91b822f86782d6638b6660b8279a2a7a60cf7e
Shohei Wada <pc@wada314.jp>
2022-01-22 01:42:09 +0900

    wakaran

--- puroro/src/internal/impls/simple/getter.rs
+++ puroro/src/internal/impls/simple/getter.rs
@@ -12,7 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::internal::methods::GetOptFieldMethodImpl;
-use crate::internal::FieldProperties;
+use crate::internal::methods::{GetFieldMethodImpl, GetOptFieldMethod};
+use crate::internal::{Bitfield, SharedBitfield};
+use crate::internal::{FieldProperties, HasField, MessageProperties};
 use crate::tags;
 use crate::Message;
+
+// (optional|required|[unlabeled]) non-message field
+impl<MP, FieldsType, SharedType, GetterType, _1, _2, _3, const NUMBER: i32>
+    GetFieldMethodImpl<
+        <FieldsType as HasField<NUMBER>>::Type,
+        SharedType,
+        tags::SimpleImpl,
+        tags::NonRepeatedLabel<_1>,
+        tags::NonMessageType<_2, _3>,
+        NUMBER,
+    > for Message<MP, tags::SimpleImpl, FieldsType, SharedType>
+where
+    FieldsType: HasField<NUMBER>,
+    MP: MessageProperties,
+    <MP as MessageProperties>::Fields<NUMBER>: FieldProperties,
+    tags::NonMessageType<_2, _3>: tags::FieldTypeTag,
+    <tags::NonMessageType<_2, _3> as tags::FieldTypeTag>::DefaultValueType:
+        Clone + Into<GetterType>,
+    for<'a> Self: 'a + GetOptFieldMethod<NUMBER, GetterType<'a> = Option<GetterType>>,
+{
+    type GetterType<'a>
+    where
+        Self: 'a,
+    = GetterType;
+    fn get(&self) -> Self::GetterType<'_> {
+        let value_opt = <Self as GetOptFieldMethod<NUMBER>>::get_opt(self);
+        value_opt.unwrap_or(
+            <<MP as MessageProperties>::Fields<NUMBER> as FieldProperties>::DEFAULT_VALUE,
+        )
+    }
+}
