313418d57605eed233dcf3953c05bbda6490eeda
Shohei Wada <pc@wada314.jp>
2022-03-13 18:16:40 +0900

    kekkyoku recursive...

--- puroro/src/deser2.rs
+++ puroro/src/deser2.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use super::deser::try_get_wire_type_and_field_number;
 use crate::internal::impls::owned::deser2::GetMaybeLastMutMessageHandler;
 use crate::internal::types::WireType;
 use crate::internal::variant::Variant;
@@ -22,13 +21,20 @@ use crate::{ErrorKind, MessageImpl, Result};
 use ::std::io::Read;
 use ::std::io::Result as IoResult;
 
-enum Task<'a> {
-    Pop,
-    Parse(&'a [u8]),
+#[derive(Clone, Default)]
+struct DeserFromSliceOptions {
+    recursion_limit: Option<usize>,
 }
 
 pub trait DeserFromSlice: MatchFieldNumber<GetMaybeLastMutMessageHandler> {
-    fn deser_from_slice(&mut self, slice: &[u8]) -> Result<()>;
+    fn deser_from_slice(&mut self, slice: &[u8]) -> Result<()> {
+        self.deser_from_slice_with_options(self, slice, DeserFromSliceOptions::default())
+    }
+    fn deser_from_slice_with_options(
+        &mut self,
+        slice: &[u8],
+        options: DeserFromSliceOptions,
+    ) -> Result<()>;
 }
 
 impl<MP, FieldsType, SharedType> DeserFromSlice
@@ -36,56 +42,25 @@ impl<MP, FieldsType, SharedType> DeserFromSlice
 where
     Self: MatchFieldNumber<GetMaybeLastMutMessageHandler>,
 {
-    fn deser_from_slice(&mut self, root_slice: &[u8]) -> Result<()> {
-        let mut tasks = vec![Task::Parse(root_slice)];
-        let mut msg_stack: Vec<&mut dyn DeserFromSlice> = vec![self];
-        while let Some(task) = tasks.pop() {
-            match task {
-                Task::Pop => {
-                    msg_stack.pop();
-                }
-                Task::Parse(slice) => {
-                    if slice.is_empty() {
-                        continue;
-                    }
-                    let msg = msg_stack
-                        .last_mut()
-                        .ok_or(ErrorKind::DeserInternalError("msg_stack is empty"))?;
-                    let (wire_type, field_number) = try_get_wire_type_and_field_number(
-                        slice.as_ref().bytes(),
-                    )?
-                    .ok_or(ErrorKind::DeserInternalError(
-                        "we have already checked if the slice is empty",
-                    ))?;
-
-                    // If the field is message type, need to go deeper
-                    if matches!(wire_type, WireType::LengthDelimited) {
-                        if let Some(submsg) = msg.match_field_number_mut(
-                            field_number,
-                            GetMaybeLastMutMessageHandler::default(),
-                        )? {
-                            let submsg_len =
-                                Variant::decode_bytes(slice.as_ref().bytes())?.to_u32()? as usize;
-
-                            // reserve the succeeding field parse for the current msg
-                            tasks.push(Task::Parse(&slice[submsg_len..]));
-
-                            // Push the child message into the msg_stack, and reserve it to pop
-                            // after the parse is done
-                            tasks.push(Task::Pop);
-                            msg_stack.push(submsg);
-
-                            // do the current message field parse next
-                            tasks.push(Task::Parse(&slice[0..submsg_len]));
-                            continue;
-                        }
-                    }
-
-                    // If the field is non-message type, just process the field and
-                    // then reserve the succeeding field parse right next
-                }
-            }
+    fn deser_from_slice_with_options(
+        &mut self,
+        mut slice: &[u8],
+        options: DeserFromSliceOptions,
+    ) -> Result<()> {
+        while !slice.is_empty() {
+            let (wire_type, number) = try_get_wire_type_and_field_number(&mut slice)?;
+            
         }
-        Ok(())
+        todo!()
     }
+}
+
+fn try_get_wire_type_and_field_number(slice: &mut &[u8]) -> Result<(WireType, i32)> {
+    let key = Variant::decode_bytes(slice.bytes())?.to_u32()?;
+    Ok((
+        WireType::try_from((key & 0x07) as i32)?,
+        (key >> 3)
+            .try_into()
+            .map_err(|_| ErrorKind::InvalidFieldNumber)?,
+    ))
 }
