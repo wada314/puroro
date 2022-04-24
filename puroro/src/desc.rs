4108f5fde05e88c4d9774b743de18e439e93637a
Shohei Wada <pc@wada314.jp>
2022-04-24 20:08:49 +0900

    desc

--- puroro/src/desc.rs
+++ puroro/src/desc.rs
@@ -11,3 +11,44 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+pub struct MessageDescriptor {
+    name: String,
+    fields: Vec<FieldDescriptor>,
+}
+
+pub struct FieldDescriptor {
+    name: String,
+    number: i32,
+    r#type: FieldType,
+}
+
+pub struct EnumDescriptor {
+    name: String,
+    values: Vec<EnumValueDescriptor>,
+}
+
+pub struct EnumValueDescriptor {
+    name: String,
+    number: i32,
+}
+
+pub enum FieldType {
+    Int32,
+    Int64,
+    UInt32,
+    UInt64,
+    SInt32,
+    SInt64,
+    Fixed32,
+    Fixed64,
+    SFixed32,
+    SFixed64,
+    Bool,
+    Enum(&'static EnumDescriptor),
+    Float,
+    Double,
+    Bytes,
+    String,
+    Message(&'static MessageDescriptor),
+}
