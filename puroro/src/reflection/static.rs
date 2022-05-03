ab9c855fcc4061f3631de721a4e4d9afd06c2412
Shohei Wada <pc@wada314.jp>
2022-05-03 17:54:44 +0900

    dame

--- puroro/src/reflection/static.rs
+++ puroro/src/reflection/static.rs
@@ -13,3 +13,17 @@
 // limitations under the License.
 
 pub mod desc;
+use crate::Result;
+use desc::FieldDescriptor;
+
+pub trait Reflection {
+    fn has_field<FD: FieldDescriptor>(&self) -> Result<bool>;
+    fn get_uint32<FD: FieldDescriptor>(&self) -> Result<u32>;
+    fn get_string<FD: FieldDescriptor>(&self) -> Result<&str>;
+    type ChildReflection<'a>: Reflection
+    where
+        Self: 'a;
+    fn get_message<FD: FieldDescriptor>(&self) -> Result<Self::ChildReflection<'_>>;
+}
+
+
--- puroro/src/reflection/static/desc.rs
+++ puroro/src/reflection/static/desc.rs
@@ -12,14 +12,67 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub trait FileDescriptor {
+use crate::tags;
 
+trait Bool {
+    type Choose<T, F>;
+}
+struct True;
+struct False;
+impl Bool for True {
+    type Choose<T, F> = T;
+}
+impl Bool for False {
+    type Choose<T, F> = F;
 }
 
-pub trait MessageDescriptor {
+trait Cell {
+    type Car;
+    type Cdr;
+}
+struct Cons<T, U>(::std::marker::PhantomData<(T, U)>);
+impl<T, U> Cell for Cons<T, U> {
+    type Car = T;
+    type Cdr = U;
+}
+struct Nil;
 
+trait CellMethod {
+    type Type;
+}
+struct HasFirst<C>(::std::marker::PhantomData<C>);
+impl<C: Cell> CellMethod for HasFirst<C> {
+    type Type = True;
+}
+impl CellMethod for HasFirst<Nil> {
+    type Type = False;
+}
+
+trait Predicate {
+    type Value: Bool;
+}
+struct NumberEquals<const X: i32, const Y: i32>;
+struct BoolToType<const X: bool>;
+impl Predicate for BoolToType<true> {
+    type Value = True;
+}
+impl Predicate for BoolToType<false> {
+    type Value = True;
+}
+impl<const X: i32, const Y: i32> Predicate for NumberEquals<X, Y> {
+    type Value = <BoolToType<{ X == Y }> as Predicate>::Value;
+}
+
+pub trait FileDescriptor {}
+
+pub trait MessageDescriptor {
+    const NAME: &'static str;
+    type Fields<const NUMBER: i32>: FieldDescriptor;
+    type Fields2: Cell;
 }
 
 pub trait FieldDescriptor {
-    
+    const NAME: &'static str;
+    const NUMBER: i32;
+    type FieldType: tags::FieldTypeTag;
 }
