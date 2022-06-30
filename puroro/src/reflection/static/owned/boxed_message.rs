abdb9610c1217ac7dd6f5ebd670092b5e39e0d9e
Shohei Wada <pc@wada314.jp>
2022-07-01 05:19:06 +0900

    hai

--- puroro/src/reflection/static/owned/boxed_message.rs
+++ puroro/src/reflection/static/owned/boxed_message.rs
@@ -60,7 +60,7 @@ where
 {
     type Target = OwnedMessage<MD>;
     fn deref(&self) -> &Self::Target {
-        unsafe { transmute::<_, &OwnedMessage<MD>>(&self.0) }
+        unsafe { transmute(&self.0) }
     }
 }
 
@@ -70,6 +70,6 @@ where
     list::Map<MdFdIntoOwnedType>: Func<MD::GetFieldListAsMdFd>,
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { transmute::<_, &mut OwnedMessage<MD>>(&mut self.0) }
+        unsafe { transmute(&mut self.0) }
     }
 }
