de2cd864d34e5f3b90fd4fbc8d113535ab823ef5
Shohei Wada <pc@wada314.jp>
2022-08-15 18:18:29 +0900

    escape

--- tests-pb/src/lib.rs
+++ tests-pb/src/lib.rs
@@ -1,20 +1,7 @@
 // A generated source code by puroro library
 #![feature(generic_associated_types)]
 // package (root)
-pub mod full_coverage2;
-pub mod full_coverage3;
-pub mod keywords;
-pub mod official_samples2;
 pub mod official_samples3;
-pub mod oneofs2;
-pub mod oneofs3;
-pub mod proto2_defaults;
-pub mod proto2_packed;
-pub mod proto3_defaults;
-pub mod proto3_packed;
-pub mod self_recursive;
-pub mod ser_tests2;
-pub mod ser_tests3;
 
 pub mod _puroro_root {
     pub use super::*;
