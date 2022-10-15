5cee679d1d43f3b029a965f8e03738e0909977f9
Shohei Wada <pc@wada314.jp>
2022-10-15 20:26:31 +0900

    fixing protobuf build

---
+++ tests-pb/src-protobuf/google/protobuf.rs
@@ -0,0 +1,11 @@
+// A generated source code by puroro library
+// package google.protobuf
+
+pub mod _puroro_root {
+    pub use super::super::_puroro_root::*;
+}
+
+pub mod _puroro {
+    pub use ::puroro::*;
+}
+pub mod compiler;
---
+++ tests-pb/src-protobuf/google/protobuf/compiler/code_generator_response/file.rs
@@ -0,0 +1,10 @@
+// A generated source code by puroro library
+// package .google.protobuf.compiler.CodeGeneratorResponse.File
+
+pub mod _puroro_root {
+    pub use super::super::_puroro_root::*;
+}
+
+pub mod _puroro {
+    pub use ::puroro::*;
+}
