c8a6ab5a1d759eabbd25c0104057a4e7aeb72b5d
wada314 <pc@wada314.jp>
2022-10-17 19:08:39 +0900

    Fix-build-deps (#60)

--- tests-pb/src/lib.rs
+++ tests-pb/src/lib.rs
@@ -2,6 +2,12 @@
 #![feature(generic_associated_types)]
 // package (root)
 
+// re-export puroro.
+pub use ::puroro;
+// re-export the primitive types in puroro namespace.
+// by using the "*", it can be hidden by the same typename explicitly defined in this file.
+pub use ::puroro::*;
+
 pub mod _puroro_root {
     pub use super::*;
 }
