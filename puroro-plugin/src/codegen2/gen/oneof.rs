17a1a2ee8d9d14de6ff07a293eba8810b71c9d3e
Shohei Wada <pc@wada314.jp>
2022-12-01 19:48:34 +0900

    splitting out...

--- puroro-plugin/src/codegen2/gen/oneof.rs
+++ puroro-plugin/src/codegen2/gen/oneof.rs
@@ -101,19 +101,16 @@ impl<T: ?Sized + Oneof> OneofExt for T {
     fn gen_union(&self) -> Result<TokenStream> {
         let union_ident = self.gen_union_ident()?;
         let case_ident = format_ident!("{}Case", self.name()?.to_camel_case());
-        let union_item_idents = try_map_fields(self, |f| f.gen_union_item_ident())?;
         let union_items = try_map_fields(self, |f| f.gen_union_item_decl())?;
-        let getter_mut_idents = try_map_fields(self, |f| f.gen_union_getter_mut_ident())?;
         let item_indices = (1..=(union_items.len() as u32)).collect::<Vec<_>>();
         let item_type_names = try_map_fields(self, |f| f.gen_generic_type_param_ident())?;
-        let field_numbers = try_map_fields(self, |f| f.number())?;
         let union_methods = try_map_fields(self, |f| f.gen_union_methods())?;
         let case_names = try_map_fields(self, |f| f.gen_case_enum_value_ident())?;
-        let borrowed_types_a = try_map_fields(self, |f| {
-            f.gen_maybe_borrowed_type(Some(format_ident!("a")))
-        })?;
         let bitfield_begin = self.bitfield_index_for_oneof()?.0;
         let bitfield_end = self.bitfield_index_for_oneof()?.1;
+
+        let oneof_union_impl = gen_oneof_union_impl(self)?;
+
         // Union includes none case, where the case enum does not.
         Ok(quote! {
             pub union #union_ident {
@@ -131,93 +128,7 @@ impl<T: ?Sized + Oneof> OneofExt for T {
                 #(#union_methods)*
             }
 
-            impl self::_puroro::internal::oneof_type::OneofUnion for #union_ident {
-                type Case = self::#case_ident;
-                type CaseRef<'a> = self::#case_ident::<#(#borrowed_types_a,)*>;
-
-                fn case_ref<B: self::_puroro::bitvec::BitSlice>(&self, bits: &B)
-                    -> ::std::option::Option<Self::CaseRef<'_>>
-                {
-                    use self::_puroro::internal::oneof_type::OneofCase;
-                    use ::std::mem::ManuallyDrop;
-                    use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
-                    use ::std::ops::Deref as _;
-                    let case_opt = <self::#case_ident as OneofCase>::from_bitslice(bits);
-                    case_opt.map(|case| {
-                        match case {
-                            #(self::#case_ident::#case_names(_) => self::#case_ident::#case_names(
-                                ManuallyDrop::deref(unsafe { &self.#union_item_idents }).get_field()
-                            ),)*
-                        }
-                    })
-                }
-
-                fn clear<B: self::_puroro::bitvec::BitSlice>(&mut self, bits: &mut B) {
-                    use self::_puroro::internal::oneof_type::OneofCase;
-                    use ::std::mem::ManuallyDrop;
-                    #[allow(unused)] use ::std::option::Option::Some;
-                    match <self::#case_ident as OneofCase>::from_bitslice(bits) {
-                        #(Some(self::#case_ident::#case_names(())) => {
-                            unsafe { ManuallyDrop::take(&mut self.#union_item_idents) };
-                        })*
-                        _ => ()
-                    }
-                    bits.set_range(#bitfield_begin..#bitfield_end, 0);
-                }
-
-                fn clone<B: self::_puroro::bitvec::BitSlice>(&self, bits: &B) -> Self {
-                    use self::_puroro::internal::oneof_type::OneofCase;
-                    #[allow(unused)] use ::std::option::Option::Some;
-                    #[allow(unused)] use ::std::clone::Clone;
-                    match <self::#case_ident as OneofCase>::from_bitslice(bits) {
-                        #(Some(self::#case_ident::#case_names(())) => Self {
-                            #union_item_idents: Clone::clone(unsafe { &self.#union_item_idents }),
-                        },)*
-                        _ => Self { _none: (), },
-                    }
-                }
-
-                fn deser_from_iter<I, B>(
-                    &mut self,
-                    bitvec: &mut B,
-                    field_data: self::_puroro::internal::ser::FieldData<I>,
-                    case: Self::Case,
-                ) -> self::_puroro::Result<()>
-                where
-                    I: ::std::iter::Iterator<Item = ::std::io::Result<u8>>,
-                    B: self::_puroro::bitvec::BitSlice,
-                {
-                    use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
-                    #[allow(unused)] use ::std::result::Result::Ok;
-                    match case {
-                        #(Self::Case::#case_names(_) => {
-                            let _ = <Self>::#getter_mut_idents(self, bitvec);
-                            unsafe { &mut self.#union_item_idents }.deser_from_iter(field_data)?;
-                        })*
-                    }
-                    Ok(())
-                }
-
-                fn ser_to_write<W, B>(&self, bitvec: &B, out: &mut W) -> self::_puroro::Result<()>
-                where
-                    W: ::std::io::Write,
-                    B: self::_puroro::bitvec::BitSlice
-                {
-                    #[allow(unused)] use ::std::option::Option::Some;
-                    #[allow(unused)] use ::std::result::Result::Ok;
-                    use self::_puroro::internal::oneof_type::OneofCase as _;
-                    use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
-                    match self::#case_ident::from_bitslice(bitvec) {
-                        #(Some(self::#case_ident::#case_names(_)) => {
-                            unsafe { &self.#union_item_idents }.ser_to_write(
-                                #field_numbers, out,
-                            )?;
-                        })*
-                        _ => (),
-                    }
-                    Ok(())
-                }
-            }
+            #oneof_union_impl
 
             impl ::std::default::Default for #union_ident {
                 fn default() -> Self {
@@ -249,4 +160,108 @@ where
     F: FnMut(Rc<dyn OneofField>) -> Result<R>,
 {
     this.fields()?.map(f).collect::<Result<Vec<_>>>()
+}
+
+fn gen_oneof_union_impl(this: &(impl ?Sized + Oneof)) -> Result<TokenStream> {
+    let union_ident = this.gen_union_ident()?;
+    let case_ident = format_ident!("{}Case", this.name()?.to_camel_case());
+    let union_item_idents = try_map_fields(this, |f| f.gen_union_item_ident())?;
+    let getter_mut_idents = try_map_fields(this, |f| f.gen_union_getter_mut_ident())?;
+    let field_numbers = try_map_fields(this, |f| f.number())?;
+    let case_names = try_map_fields(this, |f| f.gen_case_enum_value_ident())?;
+    let borrowed_types_a = try_map_fields(this, |f| {
+        f.gen_maybe_borrowed_type(Some(format_ident!("a")))
+    })?;
+    let bitfield_begin = this.bitfield_index_for_oneof()?.0;
+    let bitfield_end = this.bitfield_index_for_oneof()?.1;
+
+    Ok(quote! {
+        impl self::_puroro::internal::oneof_type::OneofUnion for #union_ident {
+            type Case = self::#case_ident;
+            type CaseRef<'a> = self::#case_ident::<#(#borrowed_types_a,)*>;
+
+            fn case_ref<B: self::_puroro::bitvec::BitSlice>(&self, bits: &B)
+                -> ::std::option::Option<Self::CaseRef<'_>>
+            {
+                use self::_puroro::internal::oneof_type::OneofCase;
+                use ::std::mem::ManuallyDrop;
+                use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
+                use ::std::ops::Deref as _;
+                let case_opt = <self::#case_ident as OneofCase>::from_bitslice(bits);
+                case_opt.map(|case| {
+                    match case {
+                        #(self::#case_ident::#case_names(_) => self::#case_ident::#case_names(
+                            ManuallyDrop::deref(unsafe { &self.#union_item_idents }).get_field()
+                        ),)*
+                    }
+                })
+            }
+
+            fn clear<B: self::_puroro::bitvec::BitSlice>(&mut self, bits: &mut B) {
+                use self::_puroro::internal::oneof_type::OneofCase;
+                use ::std::mem::ManuallyDrop;
+                #[allow(unused)] use ::std::option::Option::Some;
+                match <self::#case_ident as OneofCase>::from_bitslice(bits) {
+                    #(Some(self::#case_ident::#case_names(())) => {
+                        unsafe { ManuallyDrop::take(&mut self.#union_item_idents) };
+                    })*
+                    _ => ()
+                }
+                bits.set_range(#bitfield_begin..#bitfield_end, 0);
+            }
+
+            fn clone<B: self::_puroro::bitvec::BitSlice>(&self, bits: &B) -> Self {
+                use self::_puroro::internal::oneof_type::OneofCase;
+                #[allow(unused)] use ::std::option::Option::Some;
+                #[allow(unused)] use ::std::clone::Clone;
+                match <self::#case_ident as OneofCase>::from_bitslice(bits) {
+                    #(Some(self::#case_ident::#case_names(())) => Self {
+                        #union_item_idents: Clone::clone(unsafe { &self.#union_item_idents }),
+                    },)*
+                    _ => Self { _none: (), },
+                }
+            }
+
+            fn deser_from_iter<I, B>(
+                &mut self,
+                bitvec: &mut B,
+                field_data: self::_puroro::internal::ser::FieldData<I>,
+                case: Self::Case,
+            ) -> self::_puroro::Result<()>
+            where
+                I: ::std::iter::Iterator<Item = ::std::io::Result<u8>>,
+                B: self::_puroro::bitvec::BitSlice,
+            {
+                use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
+                #[allow(unused)] use ::std::result::Result::Ok;
+                match case {
+                    #(Self::Case::#case_names(_) => {
+                        let _ = <Self>::#getter_mut_idents(self, bitvec);
+                        unsafe { &mut self.#union_item_idents }.deser_from_iter(field_data)?;
+                    })*
+                }
+                Ok(())
+            }
+
+            fn ser_to_write<W, B>(&self, bitvec: &B, out: &mut W) -> self::_puroro::Result<()>
+            where
+                W: ::std::io::Write,
+                B: self::_puroro::bitvec::BitSlice
+            {
+                #[allow(unused)] use ::std::option::Option::Some;
+                #[allow(unused)] use ::std::result::Result::Ok;
+                use self::_puroro::internal::oneof_type::OneofCase as _;
+                use self::_puroro::internal::oneof_field_type::OneofFieldType as _;
+                match self::#case_ident::from_bitslice(bitvec) {
+                    #(Some(self::#case_ident::#case_names(_)) => {
+                        unsafe { &self.#union_item_idents }.ser_to_write(
+                            #field_numbers, out,
+                        )?;
+                    })*
+                    _ => (),
+                }
+                Ok(())
+            }
+        }
+    })
 }
