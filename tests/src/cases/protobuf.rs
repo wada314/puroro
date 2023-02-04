1ae61cec3d4b07a747cab22df9a5753e32d09c22
Shohei Wada <pc@wada314.jp>
2023-02-05 07:08:06 +0900

    remove protobuf dir, remove generated code dep from test

--- tests/src/cases/protobuf.rs
+++ tests/src/cases/protobuf.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use ::puroro_protobuf_compiled::google::protobuf::DescriptorProto;
-use ::puroro_protobuf_compiled::Message;
+use ::puroro::protobuf::google::protobuf::DescriptorProto;
+use ::puroro::protobuf::puroro::Message;
 use ::std::io::Read;
 
 #[test]
