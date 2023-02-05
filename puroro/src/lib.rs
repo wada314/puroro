607769fc9579858814f7f0023cd88feb608379c0
Shohei Wada <pc@wada314.jp>
2023-02-06 05:24:43 +0900

    Forgot to fix debug code

--- puroro/src/lib.rs
+++ puroro/src/lib.rs
@@ -28,7 +28,6 @@ pub use crate::message::Message;
 #[cfg(feature = "dev-for-protobuf-use-stable-puroro")]
 mod puroro_for_protobuf {
     pub use ::stable_puroro::*;
-    compile_error!("not here!");
 }
 #[cfg(not(feature = "dev-for-protobuf-use-stable-puroro"))]
 mod puroro_for_protobuf {
