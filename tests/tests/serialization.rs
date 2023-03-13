f58ff02a337ea5faae011de2ff365a7ee40636f5
Shohei Wada <pc@wada314.jp>
2023-03-13 18:16:52 +0900

    toottaaa

--- tests/tests/serialization.rs
+++ tests/tests/serialization.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use ::puroro::Message;
+use ::puroro::{Message, MessageView};
 use ::puroro_inline::puroro_inline;
 
 puroro_inline! {r#"
