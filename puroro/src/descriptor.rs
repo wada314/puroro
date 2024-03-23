b15746182b75536509b61f0dcc6b934d84907e25
Shohei Wada <pc@wada314.jp>
2024-03-23 23:21:02 +0900

    chigau

--- puroro/src/descriptor.rs
+++ puroro/src/descriptor.rs
@@ -13,15 +13,25 @@
 // limitations under the License.
 
 use ::std::borrow::Cow;
+use ::std::cell::OnceCell;
+
+/// Public traits for the descriptor types.
 
 pub trait FileDescriptor {
     fn name(&self) -> &str;
     fn package(&self) -> &str;
     fn dependencies(&self) -> &[&dyn FileDescriptor];
     fn public_dependencies(&self) -> &[&dyn FileDescriptor];
-    fn weak_dependencies(&self) -> &[&dyn FileDescriptor];
     fn message_types(&self) -> &[&dyn Descriptor];
     fn enum_types(&self) -> &[&dyn EnumDescriptor];
+}
+
+pub enum Edition {
+    Unknown,
+    Proto2,
+    Proto3,
+    Edition2023,
+    Edition2024,
 }
 
 pub trait Descriptor {
@@ -132,42 +142,121 @@ pub trait OneofDescriptor {
     fn fields(&self) -> &[&dyn FieldDescriptor];
 }
 
+/// Structs for the each descriptor types.
+/// These structs are strictly read-only and only knows about its children, not parent.
+/// These structs does not directly impl the traits above.
+
 pub struct FileDescriptorStruct {
-    name: Cow<'static, str>,
-    package: Cow<'static, str>,
-    message_types: Vec<DescriptorStruct>,
-    enum_types: Vec<EnumDescriptorStruct>,
+    pub name: Cow<'static, str>,
+    pub package: Cow<'static, str>,
+    pub dependency_indices: Vec<usize>,
+    pub message_types: Vec<DescriptorStruct>,
+    pub enum_types: Vec<EnumDescriptorStruct>,
+    pub syntax: Cow<'static, str>,
+    pub edition: Edition,
 }
 
 pub struct DescriptorStruct {
-    name: Cow<'static, str>,
-    full_name: Cow<'static, str>,
-    fields: Vec<FieldDescriptorStruct>,
-    oneof_decls: Vec<OneofDescriptorStruct>,
-    nested_types: Vec<DescriptorStruct>,
-    enum_types: Vec<EnumDescriptorStruct>,
+    pub name: Cow<'static, str>,
+    pub full_name: Cow<'static, str>,
+    pub fields: Vec<FieldDescriptorStruct>,
+    pub oneof_decls: Vec<OneofDescriptorStruct>,
+    pub nested_types: Vec<DescriptorStruct>,
+    pub enum_types: Vec<EnumDescriptorStruct>,
 }
 
 pub struct FieldDescriptorStruct {
-    name: Cow<'static, str>,
-    number: i32,
-    type_: self::field_descriptor::Type,
-    type_name: Cow<'static, str>,
-    label: self::field_descriptor::Label,
+    pub name: Cow<'static, str>,
+    pub number: i32,
+    pub type_: self::field_descriptor::Type,
+    pub type_name: Cow<'static, str>,
+    pub label: self::field_descriptor::Label,
 }
 
 pub struct EnumDescriptorStruct {
-    name: Cow<'static, str>,
-    values: Vec<EnumValueDescriptorStruct>,
+    pub name: Cow<'static, str>,
+    pub values: Vec<EnumValueDescriptorStruct>,
 }
 
 pub struct EnumValueDescriptorStruct {
-    name: Cow<'static, str>,
-    number: i32,
+    pub name: Cow<'static, str>,
+    pub number: i32,
 }
 
 pub struct OneofDescriptorStruct {
-    name: Cow<'static, str>,
-    field_indices: Vec<usize>,
-    is_synthetic: bool,
+    pub name: Cow<'static, str>,
+    pub field_indices: Vec<usize>,
+}
+
+/// The struct types with the "context".
+/// The context here means the path from the root of the descriptor tree to the current node.
+/// These contexted structs implements the descriptor traits above,
+/// and they can be converted from the raw struct types above.
+
+pub struct Context {
+    files: Vec<FileDescriptorStruct>,
+}
+impl From<FileDescriptorStruct> for Context {
+    fn from(f: FileDescriptorStruct) -> Self {
+        Self { files: vec![f] }
+    }
+}
+impl From<Vec<FileDescriptorStruct>> for Context {
+    fn from(files: Vec<FileDescriptorStruct>) -> Self {
+        Self { files }
+    }
+}
+impl<const N: usize> From<[FileDescriptorStruct; N]> for Context {
+    fn from(files: [FileDescriptorStruct; N]) -> Self {
+        Self {
+            files: files.into(),
+        }
+    }
+}
+impl Context {
+    fn files(&self) -> impl '_ + IntoIterator<Item = FileDescriptorWithContext> {
+        (0..(self.files.len())).map(move |i| self.file_from_index(i))
+    }
+    fn file_from_index(&self, index: usize) -> FileDescriptorWithContext {
+        FileDescriptorWithContext {
+            root: self,
+            index_in_root: index,
+            file: &self.files[index],
+            dependencies: Default::default(),
+        }
+    }
+}
+
+pub struct FileDescriptorWithContext<'a> {
+    root: &'a Context,
+    index_in_root: usize,
+    file: &'a FileDescriptorStruct,
+    dependencies: OnceCell<Vec<&'a FileDescriptorWithContext<'a>>>,
+}
+
+impl FileDescriptor for FileDescriptorWithContext<'_> {
+    fn name(&self) -> &str {
+        &self.file.name
+    }
+    fn package(&self) -> &str {
+        &self.file.package
+    }
+    fn dependencies(&self) -> &[&dyn FileDescriptor] {
+        self.dependencies.get_or_init(|| {
+            self.file
+                .dependency_indices
+                .iter()
+                .map(|&i| self.root.file_from_index(i))
+                .collect()
+        })
+    }
+    fn public_dependencies(&self) -> &[&dyn FileDescriptor] {
+        todo!()
+    }
+    fn message_types(&self) -> &[&dyn Descriptor] {
+        todo!()
+    }
+    fn enum_types(&self) -> &[&dyn EnumDescriptor] {
+        todo!()
+    }
 }
