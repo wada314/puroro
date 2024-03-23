a2ca9b6f8f659c12d94bc36b23da366e793a4864
Shohei Wada <pc@wada314.jp>
2024-03-23 18:20:46 +0900

    hoge

--- puroro/src/message.rs
+++ puroro/src/message.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 use ::futures::io::{AsyncRead, AsyncWrite};
+use ::std::alloc::{Allocator, GlobalAlloc};
+use ::std::borrow::Cow;
 
 pub trait MessageLite {}
 pub trait Message: MessageLite {}
@@ -29,6 +31,55 @@ pub trait AsyncSerializableMessage {
 
 pub trait Field {}
 
-pub struct FieldDescriptor {
-    
+pub trait FieldDescriptor {
+    fn name(&self) -> &str;
+    fn number(&self) -> i32;
+    fn label(&self) -> field_descriptor::Label;
+}
+pub mod field_descriptor {
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub enum Type {
+        DOUBLE,
+        FLOAT,
+        INT64,
+        UINT64,
+        INT32,
+        FIXED64,
+        FIXED32,
+        BOOL,
+        STRING,
+        GROUP,
+        MESSAGE,
+        BYTES,
+        UINT32,
+        ENUM,
+        SFIXED32,
+        SFIXED64,
+        SINT32,
+        SINT64,
+    }
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub enum Label {
+        OPTIONAL,
+        REQUIRED,
+        REPEATED,
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct FieldDescriptorImpl<'a> {
+    name: Cow<'a, str>,
+    number: i32,
+    label: field_descriptor::Label,
+}
+impl FieldDescriptor for FieldDescriptorImpl<'_> {
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn number(&self) -> i32 {
+        self.number
+    }
+    fn label(&self) -> field_descriptor::Label {
+        self.label
+    }
 }
