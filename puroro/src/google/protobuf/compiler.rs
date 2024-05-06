69233a6365693840c4db928431e235a5ff11daeb
Shohei Wada <pc@wada314.jp>
2024-05-06 09:31:58 +0900

    guaa

--- puroro/src/google/protobuf/compiler.rs
+++ puroro/src/google/protobuf/compiler.rs
@@ -11,3 +11,25 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use crate::untyped_message::UntypedMessage;
+use crate::variant::{Bool, Int32};
+use crate::{ErrorKind, Result};
+use ::derive_more::{Deref as DDeref, From as DFrom};
+
+#[derive(DDeref, DFrom)]
+pub struct Version<'a>(UntypedMessage<'a>);
+impl<'a> Version<'a> {
+    pub fn major(&self) -> Result<i32> {
+        Ok(self.0.scalar_variant_field::<Int32>(1)?.unwrap_or(0))
+    }
+    pub fn minor(&self) -> Result<i32> {
+        Ok(self.0.scalar_variant_field::<Int32>(2)?.unwrap_or(0))
+    }
+    pub fn patch(&self) -> Result<i32> {
+        Ok(self.0.scalar_variant_field::<Int32>(3)?.unwrap_or(0))
+    }
+    pub fn suffix(&self) -> Result<&str> {
+        Ok(self.0.field(4).as_scalar_string()?.unwrap_or(""))
+    }
+}
