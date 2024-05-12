463b6346ec439427c07bac9eff4fe5fbb6ff23ce
Shohei Wada <pc@wada314.jp>
2024-05-12 16:04:05 +0900

    kou?

--- plugin/src/main.rs
+++ plugin/src/main.rs
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.<
+// limitations under the License.
 
 use ::anyhow::Result;
 use ::ipc_channel::ipc::{bytes_channel, IpcSender};
