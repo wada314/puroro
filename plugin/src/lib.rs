7e89258ce4281973d0689a0e0271ace1a18e98b1
Shohei Wada <pc@wada314.jp>
2024-05-17 05:10:02 +0900

    hoge

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::error;
-
 use ::ipc_channel::ipc::{IpcBytesReceiver, IpcBytesSender, IpcOneShotServer};
 use ::thiserror::Error;
 
