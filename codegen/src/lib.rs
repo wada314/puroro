0409e5022b52d89bd8746d02810c2d89aaf31d08
Shohei Wada <pc@wada314.jp>
2024-05-17 20:41:53 +0900

    hai

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -15,3 +15,27 @@
 #![feature(once_cell_try)]
 
 pub mod descriptor;
+
+use ::puroro::google::protobuf::compiler::{CodeGeneratorRequest, CodeGeneratorResponse};
+use ::puroro::message::MessageLite;
+use ::thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ErrorKind {
+    #[error("Unknown compile error")]
+    CompileError,
+}
+pub type Result<T> = ::std::result::Result<T, ErrorKind>;
+
+pub fn compile(_input: &CodeGeneratorRequest) -> Result<CodeGeneratorResponse<'static>> {
+    let response = CodeGeneratorResponse::default();
+    Ok(response)
+}
+
+pub fn compile_binary(input: impl AsRef<[u8]>) -> Result<Vec<u8>> {
+    let request = CodeGeneratorRequest::deser_from_read(input.as_ref()).unwrap();
+    let response = compile(&request).unwrap();
+    let mut output_buffer = Vec::new();
+    response.write(&mut output_buffer).unwrap();
+    Ok(output_buffer)
+}
