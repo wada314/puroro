940af1c5427c2be03c11acb345ab96784b7f7704
Shohei Wada <pc@wada314.jp>
2024-05-17 20:14:30 +0900

    dousukane

--- protoc-wrapper/src/lib.rs
+++ protoc-wrapper/src/lib.rs
@@ -18,6 +18,8 @@ use ::puroro::message::MessageLite;
 use ::std::env;
 use ::std::process::{Command, ExitStatus};
 use ::std::time::Duration;
+#[cfg(feature = "on-memory")]
+use ::tempfile::TempDir;
 use ::thiserror::Error;
 use ::wait_timeout::ChildExt;
 
@@ -39,6 +41,9 @@ pub enum ErrorKind {
     ProtocTimeoutError,
     #[error("ProtocProcessError: {0}")]
     ProtocProcessError(ExitStatus),
+    #[cfg(feature = "on-memory")]
+    #[error("FileNameError")]
+    FileNameError,
 }
 pub type Result<T> = ::std::result::Result<T, ErrorKind>;
 
@@ -68,6 +73,13 @@ impl Protoc {
     }
     pub fn proto_file(mut self, path: &str) -> Self {
         self.proto_files.push(path.to_string());
+        self
+    }
+    pub fn proto_files<I>(mut self, paths: I) -> Self
+    where
+        I: IntoIterator<Item = String>,
+    {
+        self.proto_files.extend(paths);
         self
     }
     pub fn proto_path(mut self, path: &str) -> Self {
@@ -116,5 +128,87 @@ impl Protoc {
         }
 
         Ok(())
+    }
+}
+
+#[cfg(feature = "on-memory")]
+pub struct ProtocOnMemory {
+    protoc: Protoc,
+    in_files: Vec<(String, String)>,
+}
+
+impl ProtocOnMemory {
+    pub fn new() -> Self {
+        Self {
+            protoc: Protoc::new(),
+            in_files: Vec::new(),
+        }
+    }
+    pub fn protoc_path(mut self, path: &str) -> Self {
+        self.protoc = self.protoc.protoc_path(path);
+        self
+    }
+    pub fn add_file(mut self, name: &str, content: &str) -> Self {
+        self.in_files.push((name.to_string(), content.to_string()));
+        self
+    }
+    pub fn add_files<I>(mut self, files: I) -> Self
+    where
+        I: IntoIterator<Item = (String, String)>,
+    {
+        self.in_files.extend(files);
+        self
+    }
+
+    pub fn run<F>(self, func: F) -> Result<Vec<(String, String)>>
+    where
+        F: FnOnce(CodeGeneratorRequest) -> ::std::result::Result<CodeGeneratorResponse, String>,
+    {
+        let proto_dir = TempDir::new()?;
+        let out_dir = TempDir::new()?;
+        let out_dir_path = out_dir.path().to_str().unwrap().to_string();
+
+        // write the proto files to the temp dir.
+        for (name, content) in &self.in_files {
+            let path = proto_dir.path().join(&name);
+            ::std::fs::write(&path, &content)?;
+        }
+
+        // run the protoc
+        let proto_file_paths = self
+            .in_files
+            .iter()
+            .map(|(name, _)| {
+                proto_dir
+                    .path()
+                    .join(name)
+                    .to_str()
+                    .ok_or(ErrorKind::FileNameError)
+                    .map(str::to_string)
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        self.protoc
+            .out_dir(&out_dir_path)
+            .proto_path(proto_dir.path().to_str().unwrap())
+            .proto_files(proto_file_paths)
+            .run(func)?;
+
+        // read the generated files
+        let output_files = ::std::fs::read_dir(out_dir.path())?
+            .map(|entry| -> Result<_> {
+                let path = entry?.path();
+                let name = path
+                    .strip_prefix(out_dir.path())
+                    .map_err(|_| ErrorKind::FileNameError)?
+                    .to_str()
+                    .ok_or(ErrorKind::FileNameError)?
+                    .to_string();
+                let content = ::std::fs::read_to_string(&path)?;
+                Ok((name, content))
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(output_files)
     }
 }
