5f0ec88b123e66eedebde0998da5af2a87e88e56
Shohei Wada <pc@wada314.jp>
2024-05-25 11:22:33 +0900

    delete generated files

--- tests/tests/generated/com/example/tutorial.rs
+++
@@ -1,9 +0,0 @@
-//! THIS FILE IS A GENERATED FILE! DO NOT EDIT!
-//! Source(s):
-//!   hoge.proto
-pub struct Person {
-    name: String,
-    id: String,
-    email: String,
-    phones: String,
-}
--- tests/tests/generated/mod.rs
+++
@@ -1,7 +0,0 @@
-//! THIS FILE IS A GENERATED FILE! DO NOT EDIT!
-//! Source(s):
-//!   p2-example.proto
-pub mod com;
-pub struct Test1 {
-    a: String,
-}
