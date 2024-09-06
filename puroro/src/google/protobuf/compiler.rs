e8a3b9813a17c333b4c56328033d256801e6eb08
Shohei Wada <pc@wada314.jp>
2024-09-06 11:03:11 +0900

    kounarimashitaga

--- puroro/src/google/protobuf/compiler.rs
+++ puroro/src/google/protobuf/compiler.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use ::std::alloc::{Allocator, Global};
+
 use super::GenericMessageExt;
 use crate::generic_message::GenericMessage;
 use crate::google::protobuf::{FileDescriptorProto, FileDescriptorTrait};
@@ -20,45 +22,45 @@ use crate::internal::{
 };
 use crate::variant::variant_types::{Int32, UInt64};
 use crate::Result;
-use ::derive_more::{Deref as DDeref, DerefMut as DDerefMut, From as DFrom, Into as DInto};
+use ::derive_more::{Deref, DerefMut, From, Into};
 use ::ref_cast::RefCast;
 
-#[derive(DDeref, DDerefMut, DFrom, DInto, Default, Debug, RefCast)]
+#[derive(Deref, DerefMut, From, Into, Default, Debug, RefCast)]
 #[repr(transparent)]
-pub struct Version(GenericMessage);
-impl Version {
-    pub fn major(&self) -> Result<Option<i32>> {
-        self.0.field(1).try_as_scalar_variant_opt::<Int32>(false)
+pub struct Version<A: Allocator = Global>(GenericMessage<A>);
+impl<A: Allocator + Clone> Version<A> {
+    pub fn major(&self) -> i32 {
+        self.as_scalar_int32(1)
     }
-    pub fn minor(&self) -> Result<Option<i32>> {
-        self.0.field(2).try_as_scalar_variant_opt::<Int32>(false)
+    pub fn minor(&self) -> i32 {
+        self.as_scalar_int32(2)
     }
-    pub fn patch(&self) -> Result<Option<i32>> {
-        self.0.field(3).try_as_scalar_variant_opt::<Int32>(false)
+    pub fn patch(&self) -> i32 {
+        self.as_scalar_int32(3)
     }
-    pub fn suffix(&self) -> Result<&str> {
-        Ok(self.0.field(4).try_as_scalar_string_opt()?.unwrap_or(""))
+    pub fn suffix(&self) -> &str {
+        self.as_scalar_string(4)
     }
 }
 
-#[derive(DDeref, DDerefMut, DFrom, DInto, Default, Debug, RefCast)]
+#[derive(Deref, DerefMut, From, Into, Default, Debug, RefCast)]
 #[repr(transparent)]
-pub struct CodeGeneratorRequest(GenericMessage);
-impl CodeGeneratorRequest {
-    pub fn file_to_generate(&self) -> impl IntoIterator<Item = Result<&str>> {
-        self.0.field(1).try_as_repeated_string()
+pub struct CodeGeneratorRequest<A: Allocator = Global>(GenericMessage<A>);
+impl<A: Allocator + Clone> CodeGeneratorRequest<A> {
+    pub fn file_to_generate(&self) -> impl IntoIterator<Item = &str> {
+        self.as_repeated_string(1)
     }
-    pub fn parameter(&self) -> Result<Option<&str>> {
-        self.0.field(2).try_as_scalar_string_opt()
+    pub fn parameter(&self) -> &str {
+        self.as_scalar_string(2)
     }
-    pub fn proto_file(&self) -> impl Iterator<Item = Result<&FileDescriptorProto>> {
-        self.try_as_repeated_message(15)
+    pub fn proto_file(&self) -> impl Iterator<Item = &FileDescriptorProto<A>> {
+        self.as_repeated_message(15)
     }
-    pub fn source_file_descriptors(&self) -> impl Iterator<Item = Result<&FileDescriptorProto>> {
-        self.try_as_repeated_message(17)
+    pub fn source_file_descriptors(&self) -> impl Iterator<Item = &FileDescriptorProto<A>> {
+        self.as_repeated_message(17)
     }
-    pub fn compiler_version(&self) -> Result<Option<&Version>> {
-        self.try_as_scalar_message(3)
+    pub fn compiler_version(&self) -> Option<&Version<A>> {
+        self.as_scalar_message(3)
     }
 }
 
@@ -93,33 +95,30 @@ pub mod code_generator_response {
         }
     }
 
-    #[derive(DDeref, DDerefMut, DFrom, DInto, Default, Debug, RefCast)]
+    #[derive(Deref, DerefMut, From, Into, Default, Debug, RefCast)]
     #[repr(transparent)]
-    pub struct File(GenericMessage);
-    impl File {
-        pub fn name(&self) -> Result<Option<&str>> {
-            self.0.field(1).try_as_scalar_string_opt()
+    pub struct File<A: Allocator = Global>(GenericMessage<A>);
+    impl<A: Allocator + Clone> File<A> {
+        pub fn name(&self) -> &str {
+            self.as_scalar_string(1)
         }
-        pub fn insertion_point(&self) -> Result<Option<&str>> {
-            self.0.field(2).try_as_scalar_string_opt()
+        pub fn insertion_point(&self) -> &str {
+            self.as_scalar_string(2)
         }
-        pub fn content(&self) -> Result<&str> {
-            self.0
-                .field(15)
-                .try_as_scalar_string_opt()
-                .map(|opt| opt.unwrap_or(""))
+        pub fn content(&self) -> &str {
+            self.as_scalar_string(15)
         }
 
         pub fn set_name(&mut self, name: &str) -> Result<()> {
-            self.0.field(1).push_string(name);
+            self.0.field_mut(1).push_string(name);
             Ok(())
         }
         pub fn set_insertion_point(&mut self, insertion_point: &str) -> Result<()> {
-            self.0.field(2).push_string(insertion_point);
+            self.0.field_mut(2).push_string(insertion_point);
             Ok(())
         }
         pub fn set_content(&mut self, content: &str) -> Result<()> {
-            self.0.field(15).push_string(content);
+            self.0.field_mut(15).push_string(content);
             Ok(())
         }
     }
@@ -138,72 +137,49 @@ pub mod code_generator_response {
             fn set_content(&mut self, content: &str);
         }
     }
-    impl FileTrait for GenericMessage {
-        fn name(&self) -> &str {
-            self.field(1).as_scalar_string()
-        }
-        fn insertion_point(&self) -> &str {
-            self.field(2).as_scalar_string()
-        }
-        fn content(&self) -> &str {
-            self.field(15).as_scalar_string()
-        }
-    }
-    impl FileMutTrait for GenericMessage {
-        fn set_name(&mut self, name: &str) {
-            // self.field_mut(1).set_string(name)
-            todo!()
-        }
-        fn set_insertion_point(&mut self, insertion_point: &str) {
-            // self.field_mut(2).set_string(insertion_point)
-            todo!()
-        }
-        fn set_content(&mut self, content: &str) {
-            // self.field_mut(15).set_string(content)
-            todo!()
-        }
-    }
 }
 
-#[derive(DDeref, DDerefMut, DFrom, DInto, Default, Debug, RefCast)]
+#[derive(Deref, DerefMut, From, Into, Default, Debug, RefCast)]
 #[repr(transparent)]
-pub struct CodeGeneratorResponse(GenericMessage);
-impl CodeGeneratorResponse {
-    pub fn error(&self) -> Result<Option<&str>> {
-        self.0.field(1).try_as_scalar_string_opt()
+pub struct CodeGeneratorResponse<A: Allocator = Global>(GenericMessage<A>);
+impl<A: Allocator + Clone> CodeGeneratorResponse<A> {
+    pub fn error(&self) -> &str {
+        self.as_scalar_string(1)
     }
-    pub fn supported_features(&self) -> Result<Option<u64>> {
-        self.0.field(2).try_as_scalar_variant_opt::<UInt64>(false)
+    pub fn supported_features(&self) -> u64 {
+        self.0
+            .field(2)
+            .map(|f| f.as_scalar_variant::<UInt64>(false))
+            .unwrap_or_default()
     }
-    pub fn minimum_edition(&self) -> Result<Option<i32>> {
-        self.0.field(3).try_as_scalar_variant_opt::<Int32>(false)
+    pub fn minimum_edition(&self) -> i32 {
+        self.as_scalar_int32(3)
     }
-    pub fn maximum_edition(&self) -> Result<Option<i32>> {
-        self.0.field(4).try_as_scalar_variant_opt::<Int32>(false)
+    pub fn maximum_edition(&self) -> i32 {
+        self.as_scalar_int32(4)
     }
-    pub fn file(&self) -> impl Iterator<Item = Result<&code_generator_response::File>> {
-        self.try_as_repeated_message(15)
+    pub fn file(&self) -> impl Iterator<Item = &code_generator_response::File<A>> {
+        self.as_repeated_message(15)
     }
 
     pub fn set_error(&mut self, error: &str) -> Result<()> {
-        self.0.field(1).push_string(error);
+        self.0.field_mut(1).push_string(error);
         Ok(())
     }
     pub fn set_supported_features(&mut self, features: u64) -> Result<()> {
-        self.0.field(2).push_variant::<UInt64>(features);
+        self.0.field_mut(2).push_variant_from::<UInt64>(features);
         Ok(())
     }
     pub fn set_minimum_edition(&mut self, edition: i32) -> Result<()> {
-        self.0.field(3).push_variant::<Int32>(edition);
+        self.0.field_mut(3).push_variant_from::<Int32>(edition);
         Ok(())
     }
     pub fn set_maximum_edition(&mut self, edition: i32) -> Result<()> {
-        self.0.field(4).push_variant::<Int32>(edition);
+        self.0.field_mut(4).push_variant_from::<Int32>(edition);
         Ok(())
     }
-    pub fn push_file(&mut self, file: code_generator_response::File) -> Result<()> {
-        todo!();
-        // self.0.field_mut(15).push_message(&file);
+    pub fn push_file(&mut self, file: code_generator_response::File<A>) -> Result<()> {
+        self.0.field_mut(15).push_message(file.into());
         Ok(())
     }
 }
@@ -238,51 +214,3 @@ impl_message_mut_trait_for_trivial_types! {
         fn suffix_mut(&mut self) -> impl ::std::ops::DerefMut<Target = String>;
     }
 }
-
-impl VersionTrait for GenericMessage {
-    fn major(&self) -> i32 {
-        self.field(1).as_scalar_variant::<Int32>(false)
-    }
-    fn minor(&self) -> i32 {
-        self.field(2).as_scalar_variant::<Int32>(false)
-    }
-    fn patch(&self) -> i32 {
-        self.field(3).as_scalar_variant::<Int32>(false)
-    }
-    fn suffix(&self) -> &str {
-        self.field(4).as_scalar_string()
-    }
-}
-// impl VersionMutTrait for GenericMessage2 {}
-
-impl CodeGeneratorRequestTrait for GenericMessage {
-    fn file_to_generate(&self) -> impl Iterator<Item = &str> {
-        self.field(1).as_repeated_string()
-    }
-    fn parameter(&self) -> &str {
-        self.field(2).as_scalar_string()
-    }
-    fn proto_file(&self) -> impl Iterator<Item = impl FileDescriptorTrait> {
-        self.field(15).as_repeated_message()
-    }
-    fn source_file_descriptors(&self) -> impl Iterator<Item = impl FileDescriptorTrait> {
-        self.field(17).as_repeated_message()
-    }
-    fn compiler_version(&self) -> Option<impl VersionTrait> {
-        self.field(3).as_scalar_message()
-    }
-}
-// impl CodeGeneratorRequestMutTrait for GenericMessage2 {
-//     fn set_parameter(&mut self, parameter: &str) {
-//         self.field_mut(2).set_string(parameter)
-//     }
-//     fn push_proto_file(&mut self, proto_file: &GenericMessage) {
-//         self.field_mut(15).push_message(proto_file)
-//     }
-//     fn push_source_file_descriptor(&mut self, source_file_descriptor: &GenericMessage) {
-//         self.field_mut(17).push_message(source_file_descriptor)
-//     }
-//     fn set_compiler_version(&mut self, compiler_version: &GenericMessage) {
-//         self.field_mut(3).set_message(&compiler_version)
-//     }
-// }
