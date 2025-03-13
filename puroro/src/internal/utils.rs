ab50ad0c8e483bd94344661323921737297b88bc
Shohei Wada <pc@wada314.jp>
2025-03-13 13:45:17 +0900

    nurupo

--- puroro/src/internal/utils.rs
+++ puroro/src/internal/utils.rs
@@ -11,7 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-mod once_list1;
-
-pub(crate) use self::once_list1::OnceList1;
