3f06a5a430ccfc681b5eaced855f746ad06e7df4
Shohei Wada <pc@wada314.jp>
2025-06-13 01:41:57 +0900

    nanndeya

--- codegen/src/generator/gen_message_items.rs
+++ codegen/src/generator/gen_message_items.rs
@@ -108,10 +108,10 @@ impl GenMessageItems {
         let try_trait_path: Path = parse2(quote! { self::#try_trait_name })?;
 
         let blanket_impl_generators = self.create_blanket_impl_generators();
-        let trait_paths = TrySwitch::new(&view_trait_path, &try_trait_path);
+        let trait_paths = TrySwitch::new(view_trait_path, try_trait_path);
         let blanket_impls = blanket_impl_generators
             .iter()
-            .map(|g| g.generate(trait_paths.clone(), Box::new(self.fields.iter())))
+            .map(|g| g.generate(&trait_paths, Box::new(self.fields.iter())))
             .map(|r| match r {
                 Ok(vec) => Either::Left(vec.into_iter().map(Ok)),
                 Err(e) => Either::Right(once(Err(e))),
@@ -403,7 +403,7 @@ trait ImplsGenerator {
     #[throws]
     fn generate<'a>(
         &self,
-        trait_paths: TrySwitch<&Path>,
+        view_trait_paths: &TrySwitch<Path>,
         fields: Box<dyn 'a + Iterator<Item = &'a Field>>,
     ) -> Vec<Item>;
 }
